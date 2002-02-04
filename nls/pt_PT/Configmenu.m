31ab3603835d4d7893be0d114697bf800c44f891
fluxgen <fluxgen>
2002-02-04 07:45:19 +0000

    initial import

---
+++ nls/pt_PT/Configmenu.m
@@ -0,0 +1,52 @@
+$set 3 #Configmenu
+
+$ #ConfigOptions
+# Configurações
+$ #FocusModel
+# Modelo de Foco
+$ #WindowPlacement
+# Posicionamento das Janelas
+$ #ImageDithering
+# Simulação de Cores em Imagens
+$ #OpaqueMove
+# Movimento Opaco de Janelas
+$ #FullMax
+# Maximização Total
+$ #FocusNew
+# Foco em Novas Janelas
+$ #FocusLast
+# Foco na Ultima Janela Da Area de Trabalho
+$ #ClickToFocus
+# Foco na Janela Quando Clicada
+$ #SloppyFocus
+# Foco na Janela Quando Apontada
+$ #AutoRaise
+# Sobrepor Janela Quando Ativa
+$ #SmartRows
+# Posicionamento em Fila
+$ #SmartCols
+# Posicionamento em Coluna
+$ #Cascade
+# Posicionamento em Cascata
+$ #LeftRight
+# Esquerda para Direita
+$ #RightLeft
+# Direita para Esquerda
+$ #TopBottom
+# Cima para Baixo
+$ #BottomTop
+# Baixo para Cima
+$ #Tabs
+# Tabs
+$ #Icons
+# Icons
+$ #TabPlacement
+# Posicionamento da Tab
+$ #TabRotateVertical
+# Rodar Tabs Verticais
+$ #SemiSloppyFocus
+# Semi Sloppy Focus
+$ #MaxOverSlit
+# Maximizar Sobre Slit
+$ #SloppyWindowGrouping
+# Sloppy Janela Em Grupo
---
+++ nls/pt_PT/Translation.m
@@ -0,0 +1,211 @@
+$set 1 #BaseDisplay
+
+1 %s:  X erro: %s(%d) opcodes %d/%d\n  recurso 0x%lx\n
+2 %s: recebeido sinal %d\n
+3 desligando\n
+4 a abortar... a gerar ficheiro core\n
+5 BaseDisplay::BaseDisplay: conecção ao servidor X falhou.\n
+6 BaseDisplay::BaseDisplay: impossivel marcar a conexão ao display como close-on-exec\n
+7 BaseDisplay::eventLoop(): a remover má janela da trilha de eventos\n
+
+$set 2 #Basemenu
+
+1 Menu Blackbox
+
+$set 3 #Configmenu
+
+1 Configurações
+2 Modelo de Foco
+3 Posicionamento das Janelas
+4 Simulação de Cores em Imagens
+5 Movimento Opaco de Janelas
+6 Maximização Total
+7 Foco em Novas Janelas
+8 Foco na Ultima Janela Da Area de Trabalho
+9 Foco na Janela Quando Clicada
+10 Foco na Janela Quando Apontada
+11 Sobrepor Janela Quando Ativa
+12 Posicionamento em Fila
+13 Posicionamento em Coluna
+14 Posicionamento em Cascata
+15 Esquerda para Direita
+16 Direita para Esquerda
+17 Cima para Baixo
+18 Baixo para Cima
+19 Tabs
+20 Icons
+21 Posicionamento de Tab
+22 Rotação Vertical de Tabs
+23 Semi Sloppy Focus
+24 Maximize Sobre Slit
+25 Sloppy Janela de Grupo
+
+$set 4 #Icon
+
+1 Icons
+
+$set 5 #Image
+
+1 BImage::render_solid: erro a criar pixmap\n
+2 BImage::renderXImage: erro a criar XImage\n
+3 BImage::renderXImage: Imagem não suportado\n
+4 BImage::renderPixmap: erro a criar pixmap\n
+5 BImageControl::BImageControl: mapa de cores de tamanho invalido %d (%d/%d/%d) - reduzindo\n
+6 BImageControl::BImageControl: erro localizado no mapa de cores\n
+7 BImageControl::BImageControl: falha localizada na cor %d/%d/%d\n
+8 BImageControl::~BImageControl: reserva de pixmap cheia - libertando %d pixmaps\n
+9 BImageControl::renderImage: reserva cheia, a forçar limpeza\n
+10 BImageControl::getColor: color parse error: '%s'\n
+11 BImageControl::getColor: erro localizando cor: '%s'\n
+
+$set 6 #Screen
+
+1 BScreen::BScreen: um erro occorreu enquanto chamava o servidor X.\n  \
+existe outro window manager rodando no display. %s.\n
+2 BScreen::BScreen: managing screen %d using visual 0x%lx, depth %d\n
+3 BScreen::LoadStyle(): impossivel carregar fonte '%s'\n
+4 BScreen::LoadStyle(): impossivel carregar fonte padrão.\n
+5 %s: arquivo de menu vazio\n
+6 xterm
+7 Reiniciar
+8 Sair
+9 BScreen::parseMenuFile: [exec] erro, não há indicação definida no menu e/ou comando definido\n
+10 BScreen::parseMenuFile: [exit] erro, não há indicação definida no menu\n
+11 BScreen::parseMenuFile: [style] erro, não há indicação de menu e/ou nome de arquivo difinido\n
+12 BScreen::parseMenuFile: [config] erro, não há indicação definida no menu\n
+13 BScreen::parseMenuFile: [include] erro, nome de arquivo não definido\n
+14 BScreen::parseMenuFile: [include] error, '%s' não é um arquivo regular\n
+15 BScreen::parseMenuFile: [submenu] erro, não há indicação definida no menu\n
+16 BScreen::parseMenuFile: [restart] erro, não há indicação definida no menu\n
+17 BScreen::parseMenuFile: [reconfig] erro, não há indicação definida no menu\n
+18 BScreen::parseMenuFile: [stylesdir/stylesmenu] erro, não há diretorio difinido\n
+19 BScreen::parseMenuFile: [stylesdir/stylesmenu] erro, '%s' não é um diretorio\n
+20 BScreen::parseMenuFile: [stylesdir/stylesmenu] erro, '%s' não existe\n
+21 BScreen::parseMenuFile: [workspaces] erro, não há indicação definida no menu\n
+22 0: 0000 x 0: 0000
+23 X: %4d x Y: %4d
+24 W: %4d x H: %4d
+
+
+$set 7 #Slit
+
+1 Slit
+2 Direção
+3 Posicionamento
+
+$set 8 #Toolbar
+
+1 00:00000
+2 %02d/%02d/%02d
+3 %02d.%02d.%02d
+4  %02d:%02d 
+5 %02d:%02d %sm
+6 p
+7 a
+8 Toolbar
+9 Editar nome de Area de Trabalho
+10 Posicionamento
+
+$set 9 #Window
+
+
+1 BlackboxWindow::BlackboxWindow: criando 0x%lx\n
+2 BlackboxWindow::BlackboxWindow: XGetWindowAttributres falhou\n
+3 BlackboxWindow::BlackboxWindow: impossivel encontrar tela para janela 0x%lx\n
+4 Sem Nome
+5 BlackboxWindow::mapRequestEvent() para 0x%lx\n
+6 BlackboxWindow::unmapNotifyEvent() para 0x%lx\n
+7 BlackboxWindow::unmapnotifyEvent: reparent 0x%lx to root\n
+
+$set 10 #Windowmenu
+
+1 Enviar Para ...
+2 Encolher
+3 Minimizar
+4 Maximizar
+5 Focalizar
+6 Desfocalizar
+7 Sempre Visivel
+8 Kill Cliente
+9 Fechar
+10 Tab
+
+$set 11 #Workspace
+
+1 Area de Trabalho %d
+
+$set 12 #Workspacemenu
+
+1 Areas de Trabalho
+2 Nova Area de Trabalho
+3 Remover Ultima Area de Trabalho
+
+$set 13 #blackbox
+
+1 Blackbox::Blackbox: não foram encontradas telas administraveis, abortando..\n
+2 Blackbox::process_event: MapRequest para 0x%lx\n
+
+$set 14 #Common
+
+1 Sim
+2 Não
+
+3 Direção
+4 Horizontal
+5 Vertical
+
+6 Sempre Visivel
+
+7 Posicionamento
+8 No Alto a Esquerda
+9 No Centro a Esquerda
+10 Em Baixo a Esquerda
+11 No Alto e no Centro
+12 Em Baixo e no Centro
+13 No Alto a Direita
+14 No Meio a Direita
+15 Em Baixo a Direita
+16 Left Top
+17 Left Center
+18 Left Bottom
+19 Right Top
+20 Right Center
+21 Right Bottom
+22 Top Relative
+23 Bottom Relative
+24 Left Relative
+25 Right Relative
+
+26 Esconder Automaticamente
+
+$set 15 #main
+
+1 erro: '-rc' requer um argumento\n
+2 erro: '-display' requer um argumento\n
+3 aviso: não foi possivel colocar a variavel de ambiente 'DISPLAY'\n
+4 Fluxbox %s: (c) 2001 Henrik Kinnunen\n\n\
+  -display <string>\t\tusar conexão com o display.\n\
+  -rc <string>\t\t\tusar arquivo alternativo de recursos.\n\
+  -version\t\t\tmostrar versao e sair.\n\
+  -help\t\t\t\tmostrar esta ajuda e sair.\n\n
+5 Opções em tempo de compilação:\n\
+  Informações extras para problemas:\t\t\t%s\n\
+  Interlacing:\t\t\t%s\n\
+  Forma:\t\t\t%s\n\
+  Slit:\t\t\t\t%s\n\
+  8bpp Simulação ordenada de cores em imagens:\t%s\n\n
+
+$set 16 #bsetroot
+
+1 %s: erro: necessario especificar um dos seguintes argumentos: -solid, -mod, -gradient\n
+2 %s 2.0: (c) 1997-2000 Brad Hughes\n\n\
+  -display <string>        conexão com display\n\
+  -mod <x> <y>             altera pattern\n\
+  -foreground, -fg <color> altera foreground color\n\
+  -background, -bg <color> altera background color\n\n\
+  -gradient <texture>      textura gradiente\n\
+  -from <color>            cor de inicio do gradiente\n\
+  -to <color>              com do fim do gradiente\n\n\
+  -solid <color>           cor solida\n\n\
+  -help                    mostra esta ajuda e sai\n
+
