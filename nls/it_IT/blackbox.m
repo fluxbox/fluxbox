863cf91d77894f92cf9f8890366bd937479ec111
fluxgen <fluxgen>
2002-08-28 20:40:16 +0000

    minor fix

--- nls/it_IT/blackbox.m
+++ nls/it_IT/blackbox.m
@@ -1,7 +1,6 @@
 $set 13 #blackbox
 
 $ #NoManagableScreens
-# Blackbox::Blackbox: no managable screens found, aborting\n
 # Blackbox::Blackbox: nessun schermo gestibile, uscendo\n
 $ #MapRequest
 # Blackbox::process_event: MapRequest per 0x%lx\n
