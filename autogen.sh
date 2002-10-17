8b9c82b5bf97a43a61ab4ea9b981f5c79c372a99
fluxgen <fluxgen>
2002-10-17 12:12:35 +0000

    removed checking for autoconf version

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-required_version=52
-echo -n "Checking for autoconf version >= "${required_version}"......"
-ac_version=`autoconf --version|head -n 1|cut -d . -f 2`
-if [ ${ac_version} -lt ${required_version} ]; then
-	echo "No."
-	echo "You need to install autoconf version >= "${required_version}
-	exit 1
-fi
-echo "Ok."
-
 libtoolize --copy --force --automake || exit 1
 rm -f config.cache
 aclocal
