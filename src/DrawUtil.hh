1fc16d3d3d8a62a9697f4a42044b695707fe5d50
fluxgen <fluxgen>
2002-11-25 14:00:20 +0000

    removed rotate font functions

--- src/DrawUtil.hh
+++ src/DrawUtil.hh
@@ -19,15 +19,14 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// $Id: DrawUtil.hh,v 1.7 2002/11/12 22:44:17 fluxgen Exp $
+// $Id: DrawUtil.hh,v 1.8 2002/11/25 14:00:20 fluxgen Exp $
 
 #ifndef DRAWUTIL_HH
 #define DRAWUTIL_HH
 
 #include <X11/Xlib.h>
 
-namespace DrawUtil
-{
+namespace DrawUtil {
 	// note: obsolete!
 	struct Font {
 		enum FontJustify {LEFT=0, RIGHT, CENTER};
@@ -37,54 +36,6 @@ namespace DrawUtil
 		XFontStruct *fontstruct;
 		FontJustify justify;
 	};
-	
-// ----------------------------------------------------------------------
-// xvertext, Copyright (c) 1992 Alan Richardson (mppa3@uk.ac.sussex.syma)
-// ----------------------------------------------------------------------
-
-	/* *** The font structures *** */
-
-	struct BitmapStruct {
-		int			 bit_w;
-		int			 bit_h;
-
-		Pixmap bm;
-	};
-
-	struct XRotCharStruct {
-		int			 ascent;
-		int			 descent;
-		int			 lbearing;
-		int			 rbearing;
-		int			 width;
-
-		BitmapStruct	 glyph;
-	};
-
-	struct XRotFontStruct {
-		int			 dir;
-		int			 height;
-		int			 max_ascent;
-		int			 max_descent;
-		int			 max_char;
-		int			 min_char;
-		char 		*name;
-
-		XFontStruct		*xfontstruct;
-
-		DrawUtil::XRotCharStruct	 per_char[95];
-	};
-unsigned int XRotTextWidth(DrawUtil::XRotFontStruct *rotfont, const char *str, int len);
-void XRotDrawString(Display *dpy, DrawUtil::XRotFontStruct *rotfont, Drawable drawable,
-					GC gc, int x, int y, const char *str, int len);
-
-void DrawRotString(Display *display, Window w, GC gc, DrawUtil::XRotFontStruct *font,
-					unsigned int align, unsigned int text_w,
-					unsigned int size_w, unsigned int size_h,
-					unsigned int bevel_w, const char *text);
-					
-DrawUtil::XRotFontStruct *XRotLoadFont(Display *dpy, char *fontname, float angle);
-void XRotUnloadFont(Display *dpy, DrawUtil::XRotFontStruct *rotfont);
 
 }; //end namespace DrawUtil
 
