02cd8214fdc009751cd2d700a8bb1d8e98ca7b87
fluxgen <fluxgen>
2002-12-16 11:25:58 +0000

    german translation from Andreas

---
+++ nls/de_DE/BaseDisplay.m
@@ -0,0 +1,16 @@
+$set 1 #BaseDisplay
+
+$ #XError
+# %s:  X Fehler ! : %s(%d) opcodes %d/%d\n  resource 0x%lx\n
+$ #SignalCaught
+# %s: Signal %d abgefangen\n
+$ #ShuttingDown
+# Herunterfahren...\n
+$ #Aborting
+# Breche ab...core dump\n
+$ #XConnectFail
+# BaseDisplay::BaseDisplay: Verbindung mit XServer fehlgeschlagen !\n
+$ #CloseOnExecFail
+# BaseDisplay::BaseDisplay: \n
+$ #BadWindowRemove
+# BaseDisplay::eventLoop(): Entferne fehlerhafte Fenster aus der Ereignisschlange\n
