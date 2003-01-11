076e3d11854913b33e3a2822e5c81981e9cff37f
fluxgen <fluxgen>
2003-01-11 01:09:24 +0000

    executes a list of commands

---
+++ src/FbTk/MacroCommand.hh
@@ -0,0 +1,45 @@
+// MacroCommand.hh for FbTk
+// Copyright (c) 2003 Henrik Kinnunen (fluxgen at users.sourceforge.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: MacroCommand.hh,v 1.1 2003/01/11 01:09:24 fluxgen Exp $
+
+#ifndef FBTK_MACROCOMMAND_HH
+#define FBTK_MACROCOMMAND_HH
+
+#include "Command.hh"
+#include "RefCount.hh"
+
+#include <vector>
+
+namespace FbTk {
+
+/// executes a list of commands
+class MacroCommand:public Command {
+public:
+    void add(RefCount<Command> &com);
+    virtual void execute();
+private:
+    std::vector<RefCount<Command> > m_commandlist;
+};
+
+}; // end namespace FbTk
+
+#endif // FBTK_MACROCOMMAND_HH
