cefcfd37d8746f8f882e0379fe7634cc6eefb2a4
fluxgen <fluxgen>
2003-02-03 13:58:09 +0000

    first

---
+++ src/LayeredMenu.hh
@@ -0,0 +1,42 @@
+// LayeredMenu.hh for Fluxbox Window Manager
+// Copyright (c) 2003 Henrik Kinnunen (fluxgen at users.sourceforge.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: LayeredMenu.hh,v 1.1 2003/02/03 13:58:09 fluxgen Exp $
+
+#ifndef LAYEREDMENU_HH
+#define LAYEREDMENU_HH
+
+#include "Menu.hh"
+#include "XLayerItem.hh"
+
+/// a layered menu
+class LayeredMenu:public FbTk::Menu {
+public:
+    LayeredMenu(FbTk::MenuTheme &tm, int screen_num, FbTk::ImageControl &imgctrl,
+                FbTk::XLayer &layer):FbTk::Menu(tm, screen_num, imgctrl), m_layeritem(window(), layer) { }
+    void raise() { m_layeritem.raise(); }
+    void lower() { m_layeritem.lower(); }
+private:
+    FbTk::XLayerItem m_layeritem;
+};
+
+#endif // LAYEREDMENU_HH
+
