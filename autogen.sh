93220b5f1abee3057b292abb958a437ebbc707b4
fluxgen <fluxgen>
2003-05-14 16:41:48 +0000

    improved

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-libtoolize --copy --force --automake
-rm -f config.cache
-aclocal -I .
-autoheader
-automake -a
-autoconf
-echo "Done."
+# autogen script for fluxbox.
 
+dothis() {
+    echo "Executing:  $*"
+    echo
+    if ! $* ;then
+        echo -e '\n ERROR: Carefully read the error message and'
+        echo      '        try to figure out what went wrong.'
+        exit 1
+    fi
+}
 
+dothis libtoolize --copy --force --automake
+dothis rm -f config.cache
+dothis aclocal -I .
+dothis autoheader
+dothis automake -a
+dothis autoconf
+
+echo 'Succes, now continue with ./configure'
+echo 'Use configure --help for detailed help.'
