695d926b2f56a602b62773f2a6aa4eaf2c5c40b2
fluxgen <fluxgen>
2003-06-30 14:35:11 +0000

    commands on current window

---
+++ src/CurrentWindowCmd.cc
@@ -0,0 +1,77 @@
+// CurrentWindowCmd.cc for Fluxbox - an X11 Window manager
+// Copyright (c) 2003 Henrik Kinnunen (fluxgen{<a*t>}users.sourceforge.net)
+//                and Simon Bowden (rathnor at users.sourceforge.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: CurrentWindowCmd.cc,v 1.1 2003/06/30 14:35:11 fluxgen Exp $
+
+#include "CurrentWindowCmd.hh"
+
+#include "fluxbox.hh"
+#include "Window.hh"
+#include "Screen.hh"
+
+CurrentWindowCmd::CurrentWindowCmd(Action act):m_action(act) { }
+
+void CurrentWindowCmd::execute() {
+    Fluxbox *fb = Fluxbox::instance();
+    if (fb->getFocusedWindow() != 0)
+        (*fb->getFocusedWindow().*m_action)();
+}
+
+
+void KillWindowCmd::real_execute() {
+    XKillClient(FbTk::App::instance()->display(), window().clientWindow());
+}
+
+void SendToWorkspaceCmd::real_execute() {
+    if (m_workspace_num > 0 && m_workspace_num < window().screen().getNumberOfWorkspaces())
+        window().screen().sendToWorkspace(m_workspace_num, &window());
+}
+
+void WindowHelperCmd::execute() {
+    if (Fluxbox::instance()->getFocusedWindow())
+        real_execute();
+}
+
+FluxboxWindow &WindowHelperCmd::window() {
+    return *Fluxbox::instance()->getFocusedWindow();
+}
+
+MoveLeftCmd::MoveLeftCmd(int step_size):MoveHelper(step_size) { }
+void MoveLeftCmd::real_execute() {
+    window().move(window().x() - stepSize(), window().y());
+}
+
+MoveRightCmd::MoveRightCmd(int step_size):MoveHelper(step_size) { }
+void MoveRightCmd::real_execute() {
+    window().move(window().x() + stepSize(), window().y());
+}
+
+MoveDownCmd::MoveDownCmd(int step_size):MoveHelper(step_size) { }
+void MoveDownCmd::real_execute() {
+    window().move(window().x(), window().y() + stepSize());
+}
+
+MoveUpCmd::MoveUpCmd(int step_size):MoveHelper(step_size) { }
+void MoveUpCmd::real_execute() {
+    window().move(window().x(), window().y() - stepSize());
+}
+
---
+++ src/CurrentWindowCmd.hh
@@ -0,0 +1,110 @@
+// CurrentWindowCmd.hh for Fluxbox - an X11 Window manager
+// Copyright (c) 2003 Henrik Kinnunen (fluxgen{<a*t>}users.sourceforge.net)
+//                and Simon Bowden (rathnor at users.sourceforge.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: CurrentWindowCmd.hh,v 1.1 2003/06/30 14:35:11 fluxgen Exp $
+
+#ifndef CURRENTWINDOWCMD_HH
+#define CURRENTWINDOWCMD_HH
+
+#include "Command.hh"
+
+class FluxboxWindow;
+/// command that calls FluxboxWindow::<the function> on execute()
+/// similar to FbTk::SimpleCommand<T>
+class CurrentWindowCmd: public FbTk::Command {
+public:
+    typedef void (FluxboxWindow::* Action)();
+    explicit CurrentWindowCmd(Action action);
+    void execute();
+private:
+    Action m_action;
+};
+
+/// helper class for window commands
+/// calls real_execute if there's a focused window or a window in button press/release window
+class WindowHelperCmd: public FbTk::Command {
+public:
+    void execute();
+
+protected:
+
+    FluxboxWindow &window();
+    virtual void real_execute() = 0;
+
+};
+
+class KillWindowCmd: public WindowHelperCmd {
+protected:
+    void real_execute();
+};
+
+class SendToWorkspaceCmd: public WindowHelperCmd {
+public:
+    explicit SendToWorkspaceCmd(int workspace_num):m_workspace_num(workspace_num) { }
+protected:
+    void real_execute();
+private:
+    const int m_workspace_num;
+};
+
+class MoveHelper: public WindowHelperCmd {
+public:
+    explicit MoveHelper(int step_size):m_step_size(step_size) { }
+protected:
+    int stepSize() const { return m_step_size; }
+
+private:
+    const int m_step_size;
+};
+/// move window to left
+class MoveLeftCmd: public MoveHelper {
+public:
+    explicit MoveLeftCmd(int step_size);
+protected:
+    void real_execute();
+};
+
+/// move window to right
+class MoveRightCmd: public MoveHelper {
+public:
+    explicit MoveRightCmd(int step_size);
+protected:
+    void real_execute();
+};
+
+/// move window up
+class MoveUpCmd: public MoveHelper {
+public:
+    explicit MoveUpCmd(int step_size);
+protected:
+    void real_execute();
+};
+
+/// move window down
+class MoveDownCmd: public MoveHelper {
+public:
+    explicit MoveDownCmd(int step_size);
+protected:
+    void real_execute();
+};
+
+#endif // CURRENTWINDOWCMD_HH
