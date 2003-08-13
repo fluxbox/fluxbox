cb0c4213a4359ab65e7a383dd22f8bf21ee6044f
fluxgen <fluxgen>
2003-08-13 09:59:25 +0000

    border and setAntialias

--- src/IconbarTheme.cc
+++ src/IconbarTheme.cc
@@ -8,6 +8,9 @@ IconbarTheme::IconbarTheme(int screen_num,
     m_focused_texture(*this, name + ".focused", altname + ".Focused"),
     m_unfocused_texture(*this, name + ".unfocused", altname + ".Unfocused"),
     m_empty_texture(*this, name + ".empty", altname + ".Empty"),
+    m_focused_border(*this, name + ".focused", altname + ".Focused"),
+    m_unfocused_border(*this, name + ".unfocused", altname + ".Unfocused"),
+    m_border(*this, name, altname),
     m_focused_text(*this, name + ".focused", altname + ".Focused"),
     m_unfocused_text(*this, name + ".unfocused", altname + ".Unfocused") {
 
@@ -17,8 +20,15 @@ IconbarTheme::IconbarTheme(int screen_num,
 IconbarTheme::~IconbarTheme() {
 
 }
+
+
 void IconbarTheme::reconfigTheme() {
     m_focused_text.update();
     m_unfocused_text.update();
+}
+
+void IconbarTheme::setAntialias(bool value) {
+    m_focused_text.setAntialias(value);
+    m_unfocused_text.setAntialias(value);
 }
 
--- src/IconbarTheme.hh
+++ src/IconbarTheme.hh
@@ -3,7 +3,9 @@
 
 #include "FbTk/Theme.hh"
 #include "FbTk/Texture.hh"
+
 #include "TextTheme.hh"
+#include "BorderTheme.hh"
 
 class IconbarTheme:public FbTk::Theme {
 public:
@@ -11,15 +13,23 @@ public:
     virtual ~IconbarTheme();
 
     void reconfigTheme();
+
+    void setAntialias(bool antialias);
+
     const TextTheme &focusedText() const { return m_focused_text; }
     const TextTheme &unfocusedText() const { return m_unfocused_text; }
 
+    const BorderTheme &focusedBorder() const { return m_focused_border; }
+    const BorderTheme &unfocusedBorder() const { return m_unfocused_border; }
+    const BorderTheme &border() const { return m_border; }
+    
     const FbTk::Texture &focusedTexture() const { return *m_focused_texture; }
     const FbTk::Texture &unfocusedTexture() const { return *m_unfocused_texture; }
     const FbTk::Texture &emptyTexture() const { return *m_empty_texture; }
-    
+
 private:
     FbTk::ThemeItem<FbTk::Texture> m_focused_texture, m_unfocused_texture, m_empty_texture;
+    BorderTheme m_focused_border, m_unfocused_border, m_border;
     TextTheme m_focused_text, m_unfocused_text;
 };
 
