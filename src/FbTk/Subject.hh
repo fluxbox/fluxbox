4044ac424eaa512a832a6d86082c57f4968d6b4a
fluxgen <fluxgen>
2003-08-19 16:03:52 +0000

    copyright update

--- src/FbTk/Subject.hh
+++ src/FbTk/Subject.hh
@@ -1,5 +1,5 @@
 // Subject.hh for FbTk
-// Copyright (c) 2002 Henrik Kinnunen (fluxgen@fluxbox.org)
+// Copyright (c) 2002-2003 Henrik Kinnunen (fluxgen<at>users.sourceforge.net)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -19,7 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// $Id: Subject.hh,v 1.2 2003/08/11 15:01:50 fluxgen Exp $
+// $Id: Subject.hh,v 1.3 2003/08/19 16:03:52 fluxgen Exp $
 
 #ifndef FBTK_SUBJECT_HH
 #define FBTK_SUBJECT_HH
