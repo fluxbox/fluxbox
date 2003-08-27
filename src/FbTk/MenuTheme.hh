097fcff44f305dcd4c10cc6d2cac9ad5092d5e94
fluxgen <fluxgen>
2003-08-27 14:07:16 +0000

    added pixmap for bullet and selected and unselected

--- src/FbTk/MenuTheme.hh
+++ src/FbTk/MenuTheme.hh
@@ -19,7 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// $Id: MenuTheme.hh,v 1.7 2003/07/19 03:59:56 rathnor Exp $
+// $Id: MenuTheme.hh,v 1.8 2003/08/27 14:07:16 fluxgen Exp $
 
 #ifndef FBTK_MENUTHEME_HH
 #define FBTK_MENUTHEME_HH
@@ -30,6 +30,8 @@
 #include "Texture.hh"
 #include "Text.hh"
 #include "Subject.hh"
+#include "PixmapWithMask.hh"
+#include "GContext.hh"
 
 namespace FbTk {
 
@@ -58,6 +60,10 @@ public:
     const FbTk::Texture &frameTexture() const { return *frame; }
     const FbTk::Texture &hiliteTexture() const { return *hilite; }
     ///@}
+
+    const FbTk::PixmapWithMask &bulletPixmap() const { return *m_bullet_pixmap; }
+    const FbTk::PixmapWithMask &selectedPixmap() const { return *m_selected_pixmap; }
+    const FbTk::PixmapWithMask &unselectedPixmap() const { return *m_unselected_pixmap; }
     /**
        @name fonts
     */
@@ -75,11 +81,11 @@ public:
        @name graphic contexts
     */
     ///@{
-    GC titleTextGC() const { return t_text_gc; }
-    GC frameTextGC() const { return f_text_gc; }
-    GC hiliteTextGC() const { return h_text_gc; }
-    GC disableTextGC() const { return d_text_gc; }
-    GC hiliteGC() const { return hilite_gc; }
+    GC titleTextGC() const { return t_text_gc.gc(); }
+    GC frameTextGC() const { return f_text_gc.gc(); }
+    GC hiliteTextGC() const { return h_text_gc.gc(); }
+    GC disableTextGC() const { return d_text_gc.gc(); }
+    GC hiliteGC() const { return hilite_gc.gc(); }
     ///@}
     BulletType bullet() const { return *m_bullet; }
     FbTk::Justify bulletPos() const { return *bullet_pos; }
@@ -106,10 +112,10 @@ private:
     FbTk::ThemeItem<unsigned int> m_border_width;
     FbTk::ThemeItem<unsigned int> m_bevel_width;
     FbTk::ThemeItem<FbTk::Color> m_border_color;
-
+    FbTk::ThemeItem<FbTk::PixmapWithMask> m_bullet_pixmap, m_selected_pixmap, m_unselected_pixmap;
 
     Display *m_display;
-    GC t_text_gc, f_text_gc, h_text_gc, d_text_gc, hilite_gc;
+    FbTk::GContext t_text_gc, f_text_gc, h_text_gc, d_text_gc, hilite_gc;
     FbTk::Subject m_theme_change_sig;
 
     unsigned char m_alpha;
