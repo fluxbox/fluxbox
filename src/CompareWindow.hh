bad4ea701d876a6deeb555b36b447bf48d22cc5a
fluxgen <fluxgen>
2004-02-27 12:18:26 +0000

    just a typedef

---
+++ src/CompareWindow.hh
@@ -0,0 +1,9 @@
+#ifndef COMPAREWINDOW_HH
+#define COMPAREWINDOW_HH
+
+#include "FbTk/CompareEqual.hh"
+#include "FbTk/FbWindow.hh"
+
+typedef FbTk::CompareEqual_base<FbTk::FbWindow, Window> CompareWindow;
+
+#endif // COMPAREWINDOW_HH
