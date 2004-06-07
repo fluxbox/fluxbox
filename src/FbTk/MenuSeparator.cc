c7f696db319ff1f93e5561e10ed202fe55aec9ce
fluxgen <fluxgen>
2004-06-07 21:05:16 +0000

    draws a separator

---
+++ src/FbTk/MenuSeparator.cc
@@ -0,0 +1,48 @@
+// MenuSeparator.cc for FbTk - Fluxbox ToolKit
+// Copyright (c) 2004 Henrik Kinnunen (fluxgen at users.sourceforge.net)
+//                and Simon Bowden (rathnor at users.sourceforge.net)
+//                 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: MenuSeparator.cc,v 1.1 2004/06/07 21:05:16 fluxgen Exp $
+
+#include "MenuSeparator.hh"
+
+#include "GContext.hh"
+#include "MenuTheme.hh"
+#include "FbDrawable.hh"
+
+namespace FbTk {
+
+void MenuSeparator::draw(FbDrawable &drawable, 
+                         const MenuTheme &theme,
+                         bool highlight,
+                         int x, int y,
+                         unsigned int width, unsigned int height) const {
+    const GContext &tgc =
+        (highlight ? theme.hiliteTextGC() :
+         (isEnabled() ? theme.frameTextGC() : theme.disableTextGC() ) );
+
+    drawable.drawRectangle(tgc.gc(),
+                           x + theme.bevelWidth() + height + 1, y + height / 2,
+        width - ((theme.bevelWidth() + height) * 2) - 1, 0);
+}
+
+}
+
