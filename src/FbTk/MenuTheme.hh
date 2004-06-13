8afaff036446dd5494f9f0424eb097eb24083e5e
fluxgen <fluxgen>
2004-06-13 00:33:06 +0000

    inline functions

--- src/FbTk/MenuTheme.hh
+++ src/FbTk/MenuTheme.hh
@@ -19,7 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// $Id: MenuTheme.hh,v 1.14 2004/06/07 21:02:49 fluxgen Exp $
+// $Id: MenuTheme.hh,v 1.15 2004/06/13 00:33:06 fluxgen Exp $
 
 #ifndef FBTK_MENUTHEME_HH
 #define FBTK_MENUTHEME_HH
@@ -54,72 +54,72 @@ public:
        @name text colors
     */
     ///@{
-    const FbTk::Color &titleTextColor() const { return *t_text; }
-    const FbTk::Color &frameTextColor() const { return *f_text; }
-    const FbTk::Color &highlightTextColor() const { return *h_text; }
-    const FbTk::Color &disableTextColor() const { return *d_text; }
+    inline const FbTk::Color &titleTextColor() const { return *t_text; }
+    inline const FbTk::Color &frameTextColor() const { return *f_text; }
+    inline const FbTk::Color &highlightTextColor() const { return *h_text; }
+    inline const FbTk::Color &disableTextColor() const { return *d_text; }
     ///@}
     /**
        @name textures
     */
     ///@{
-    const FbTk::Texture &titleTexture() const { return *title; }
-    const FbTk::Texture &frameTexture() const { return *frame; }
-    const FbTk::Texture &hiliteTexture() const { return *hilite; }
+    inline const FbTk::Texture &titleTexture() const { return *title; }
+    inline const FbTk::Texture &frameTexture() const { return *frame; }
+    inline const FbTk::Texture &hiliteTexture() const { return *hilite; }
     ///@}
 
-    const FbTk::PixmapWithMask &bulletPixmap() const { return *m_bullet_pixmap; }
-    const FbTk::PixmapWithMask &selectedPixmap() const { return *m_selected_pixmap; }
-    const FbTk::PixmapWithMask &unselectedPixmap() const { return *m_unselected_pixmap; }
+    inline const FbTk::PixmapWithMask &bulletPixmap() const { return *m_bullet_pixmap; }
+    inline const FbTk::PixmapWithMask &selectedPixmap() const { return *m_selected_pixmap; }
+    inline const FbTk::PixmapWithMask &unselectedPixmap() const { return *m_unselected_pixmap; }
     /**
        @name fonts
     */
     ///@{
-    const FbTk::Font &titleFont() const { return *titlefont; }
-    FbTk::Font &titleFont() { return *titlefont; }
-    const FbTk::Font &frameFont() const { return *framefont; }
-    FbTk::Font &frameFont() { return *framefont; }
+    inline const FbTk::Font &titleFont() const { return *titlefont; }
+    inline FbTk::Font &titleFont() { return *titlefont; }
+    inline const FbTk::Font &frameFont() const { return *framefont; }
+    inline FbTk::Font &frameFont() { return *framefont; }
     ///@}
 
-    FbTk::Justify frameFontJustify() const { return *framefont_justify; }
-    FbTk::Justify titleFontJustify() const { return *titlefont_justify; }
+    inline FbTk::Justify frameFontJustify() const { return *framefont_justify; }
+    inline FbTk::Justify titleFontJustify() const { return *titlefont_justify; }
 	
     /**
        @name graphic contexts
     */
     ///@{
-    const GContext &titleTextGC() const { return t_text_gc; }
-    const GContext &frameTextGC() const { return f_text_gc; }
-    const GContext &hiliteTextGC() const { return h_text_gc; }
-    const GContext &disableTextGC() const { return d_text_gc; }
-    const GContext &hiliteGC() const { return hilite_gc; }
-    GContext &titleTextGC() { return t_text_gc; }
-    GContext &frameTextGC() { return f_text_gc; }
-    GContext &hiliteTextGC() { return h_text_gc; }
-    GContext &disableTextGC() { return d_text_gc; }
-    GContext &hiliteGC() { return hilite_gc; }
+    inline const GContext &titleTextGC() const { return t_text_gc; }
+    inline const GContext &frameTextGC() const { return f_text_gc; }
+    inline const GContext &hiliteTextGC() const { return h_text_gc; }
+    inline const GContext &disableTextGC() const { return d_text_gc; }
+    inline const GContext &hiliteGC() const { return hilite_gc; }
+    inline GContext &titleTextGC() { return t_text_gc; }
+    inline GContext &frameTextGC() { return f_text_gc; }
+    inline GContext &hiliteTextGC() { return h_text_gc; }
+    inline GContext &disableTextGC() { return d_text_gc; }
+    inline GContext &hiliteGC() { return hilite_gc; }
     ///@}
-    BulletType bullet() const { return *m_bullet; }
-    FbTk::Justify bulletPos() const { return *bullet_pos; }
+    inline BulletType bullet() const { return *m_bullet; }
+    inline FbTk::Justify bulletPos() const { return *bullet_pos; }
 
-    unsigned int titleHeight() const { return *m_title_height; }
-    unsigned int itemHeight() const { return *m_item_height; }
-    unsigned int borderWidth() const { return *m_border_width; }
-    unsigned int bevelWidth() const { return *m_bevel_width; }
+    inline unsigned int titleHeight() const { return *m_title_height; }
+    inline unsigned int itemHeight() const { return *m_item_height; }
+    inline unsigned int borderWidth() const { return *m_border_width; }
+    inline unsigned int bevelWidth() const { return *m_bevel_width; }
 
     inline unsigned char alpha() const { return m_alpha; }
-    void setAlpha(unsigned char alpha) { m_alpha = alpha; }
+    inline void setAlpha(unsigned char alpha) { m_alpha = alpha; }
     // this isn't actually a theme item
     // but we'll let it be here for now, until there's a better way to
     // get resources into menu
-    void setMenuMode(MenuMode mode) { m_menumode = mode; }
-    MenuMode menuMode() const { return m_menumode; }
-    void setDelayOpen(int msec) { m_delayopen = msec; }
-    void setDelayClose(int msec) { m_delayclose = msec; }
-    int delayOpen() const { return m_delayopen; }
-    int delayClose() const { return m_delayclose; }
+    inline void setMenuMode(MenuMode mode) { m_menumode = mode; }
+    inline MenuMode menuMode() const { return m_menumode; }
+    inline void setDelayOpen(int msec) { m_delayopen = msec; }
+    inline void setDelayClose(int msec) { m_delayclose = msec; }
+    inline int delayOpen() const { return m_delayopen; }
+    inline int delayClose() const { return m_delayclose; }
     
-    const FbTk::Color &borderColor() const { return *m_border_color; }
+    inline const FbTk::Color &borderColor() const { return *m_border_color; }
 
 private:
     FbTk::ThemeItem<FbTk::Color> t_text, f_text, h_text, d_text;
