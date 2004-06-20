f6406c88d22d446cf76235f77403caa15ce4c413
rathnor <rathnor>
2004-06-20 14:16:14 +0000

    last nls tweak for now - it works this time

--- nls/C/Translation.m
+++ nls/C/Translation.m
@@ -472,3 +472,477 @@ $set 0x13 #main
 
 $set 0xd #mainWarnDisplay
 
+
+$set 0x1 #Align
+
+0x1 BottomCenter
+0x2 Bottom Left
+0x3 Bottom Right
+0x4 Horizontal
+0x5 Left
+0x6 Left Bottom
+0x7 Left Center
+0x8 Left Top
+0x9 Relative
+0xa Right
+0xb Right Bottom
+0xc Right Center
+0xd Right Top
+0xe Top Center
+0xf Top Left
+0x10 Top Right
+0x11 Vertical
+
+$set 0x2 #BaseDisplay
+
+0x1 Aborting... dumping core\n
+0x2 Shutting Down\n
+0x3 %s:      signal %d caught\n
+
+$set 0x3 #Common
+
+0x1 Alpha
+0x2 Auto hide
+0x3 Compiled
+0x4 Compiled options
+0x5 Compiler
+0x6 Compiler version
+0x7 init
+0x8 keys
+0x9 menu
+0xa style
+0xb Defaults
+0xc disabled
+0xd Error
+0xe Fluxbox version
+0xf Maximize Over
+0x10 Visible
+
+$set 0x4 #Configmenu
+
+0x1 AntiAlias
+0x2 Auto Raise
+0x3 Click Raises
+0x4 Click to Focus
+0x5 Decorate Transient Windows
+0x6 Desktop MouseWheel Switching
+0x7 Focus Window on Workspace Change
+0x8 Focus Model
+0x9 Focus New Windows
+0xa Full Maximization
+0xb Image Dithering
+0xc Opaque Window Moving
+0xd Semi Sloppy Focus
+0xe Sloppy Focus
+0xf Workspace Warping
+
+$set 0x5 #Ewmh
+
+0x1 Fatal: Out of memory, can't allocate for EWMH client list
+
+$set 0x6 #FbTkError
+
+0x1 Warning! Height > 3200 setting Height = 3200
+0x2 Warning! Width > 3200 setting Width = 3200
+0x3 Warning: can't load fallback font
+0x4 Allocation error.
+0x5 Error creating pixmap
+0x6 Cant create XImage
+0x7 Failed to read
+0x8 Warning: Failed to find valid format for alpha.
+0x9 Warning: Failed to create alpha picture.
+0xa Warning: Failed to create alpha pixmap.
+0xb Failed to find format for screen(%d)
+0xc Out of memory
+0xd Out of memory while allocating blue buffer.
+0xe Out of memory while allocating green buffer.
+0xf Out of memory while allocating red buffer.
+0x10 Failed to read theme item
+0x11 unsupported visual
+0x12 Setting default value
+
+$set 0x7 #Fluxbox
+
+0x1 rc filename is invalid!
+0x2 Failed to load groupfile
+0x3 Failed to load database
+0x4 Retrying with
+0x5 Warning! Could not find screen to map window on!
+0x6 Can't create %s directory
+0x7 Couldn't find screens to manage.\nMake sure you don't have another window manager running.
+0x8 Error parsing regular expression
+0x9 Fatal! There can only one instance of fluxbox class.
+0xa Can not connect to X server.\nMake sure you started X before you start Fluxbox.
+0xb Warning: X server does not support locale
+0xc Warning: cannot set locale modifiers
+
+$set 0x8 #Gnome
+
+0x1 Fatal: Out of memory, can't allocate for GNOME client list
+
+$set 0x9 #Keys
+
+0x1 Keys: Error on line
+0x2 Keys: Failed to merge keytree!
+0x3 Keys: Invalid key/modifier on line
+
+$set 0xa #Menu
+
+0x1 Configuration
+0x2 Fluxbox default menu
+0x3 Exit
+0x4 Icons
+0x5 Layer...
+0x6 On Head...
+0x7 Placement
+0x8 Reload Config
+0x9 Restart
+0xa xterm
+
+$set 0xb #Remember
+
+0x1 Decorations
+0x2 Dimensions
+0x3 Jump to workspace
+0x4 Layer
+0x5 Remember...
+0x6 Position
+0x7 Save on close
+0x8 Shaded
+0x9 Sticky
+0xa Unknown apps key
+0xb Workspace
+
+$set 0xc #Screen
+
+0x1 BScreen::BScreen: an error occured while querying the X server.\n	another window manager already running on display
+0x2 W: %4d x H: %4d
+0x3 W: 0000 x H: 0000
+0x4 BScreen::BScreen: managing screen %d using visual 0x%lx, depth %d\n
+0x5 X: %4d x Y: %4d
+0x6 0: 0000 x 0: 0000
+
+$set 0xd #Slit
+
+0x1 Clients
+0x2 Cycle Down
+0x3 Cycle Up
+0x4 Slit Direction
+0x5 Slit Layer
+0x6 Slit on Head
+0x7 Slit Placement
+0x8 Slit
+
+$set 0xe #Toolbar
+
+0x1 Edit current workspace name
+0x2 Iconbar Mode
+0x3 All Windows
+0x4 Icons
+0x5 None
+0x6 Workspace
+0x7 WorkspaceIcons
+0x8 Toolbar Layer
+0x9 Toolbar on Head
+0xa Toolbar Placement
+0xb Toolbar
+0xc Toolbar width percent
+
+$set 0xf #Window
+
+0x1 Unnamed
+
+$set 0x10 #Windowmenu
+
+0x1 Close
+0x2 Iconify
+0x3 Layer
+0x4 Lower
+0x5 Maximize
+0x6 Raise
+0x7 Send To ...
+0x8 Shade
+0x9 Stick
+
+$set 0x11 #Workspace
+
+0x1 Workspace %d
+0x2 Workspaces
+0x3 New Workspace
+0x4 Remove Last
+
+$set 0x12 #bsetroot
+
+0x1 %s: error: must specify one of: -solid, -mod, -gradient\n
+0x2 Couldn't create pixmap atoms, giving up!
+0x3 %s 2.0: (c) 1997-2000 Brad Hughes\n\n\
+-display <string>        display connection\n\
+-mod <x> <y>             modula pattern\n\
+-foreground, -fg <color> modula foreground color\n\
+-background, -bg <color> modula background color\n\n\
+-gradient <texture>      gradient texture\n\
+-from <color>            gradient start color\n\
+-to <color>              gradient end color\n\n\
+-solid <color>           solid color\n\n\
+-help                    print this help text and exit\n
+
+$set 0x13 #main
+
+0x1 error: '-display' requires an argument\n
+0x2 Bad Alloc
+0x3 Bad cast
+0x4 Out of range
+0x5 Runtime error
+0x6 Standard Exception
+0x7 Unknown error
+0x8 error: '-log' needs an argument
+0x9 Log File
+0xa Logging to
+0xb error: '-rc' requires an argument\n
+0xc Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
+-display <string>\t\tuse display connection.\n\
+-rc <string>\t\t\tuse alternate resource file.\n\
+-version\t\t\tdisplay version and exit.\n\
+-info\t\t\t\tdisplay some useful information.\n\
+-log <filename>\t\t\tlog output to file.\n\
+-help\t\t\t\tdisplay this help text and exit.\n\n
+
+$set 0xd #mainWarnDisplay
+
+
+$set 0x1 #Align
+
+0x1 BottomCenter
+0x2 Bottom Left
+0x3 Bottom Right
+0x4 Horizontal
+0x5 Left
+0x6 Left Bottom
+0x7 Left Center
+0x8 Left Top
+0x9 Relative
+0xa Right
+0xb Right Bottom
+0xc Right Center
+0xd Right Top
+0xe Top Center
+0xf Top Left
+0x10 Top Right
+0x11 Vertical
+
+$set 0x2 #BaseDisplay
+
+0x1 Aborting... dumping core\n
+0x2 Shutting Down\n
+0x3 %s:      signal %d caught\n
+
+$set 0x3 #Common
+
+0x1 Alpha
+0x2 Auto hide
+0x3 Compiled
+0x4 Compiled options
+0x5 Compiler
+0x6 Compiler version
+0x7 init
+0x8 keys
+0x9 menu
+0xa style
+0xb Defaults
+0xc disabled
+0xd Error
+0xe Fluxbox version
+0xf Maximize Over
+0x10 Visible
+
+$set 0x4 #Configmenu
+
+0x1 AntiAlias
+0x2 Auto Raise
+0x3 Click Raises
+0x4 Click to Focus
+0x5 Decorate Transient Windows
+0x6 Desktop MouseWheel Switching
+0x7 Focus Window on Workspace Change
+0x8 Focus Model
+0x9 Focus New Windows
+0xa Full Maximization
+0xb Image Dithering
+0xc Opaque Window Moving
+0xd Semi Sloppy Focus
+0xe Sloppy Focus
+0xf Workspace Warping
+
+$set 0x5 #Ewmh
+
+0x1 Fatal: Out of memory, can't allocate for EWMH client list
+
+$set 0x6 #FbTkError
+
+0x1 Warning! Height > 3200 setting Height = 3200
+0x2 Warning! Width > 3200 setting Width = 3200
+0x3 Warning: can't load fallback font
+0x4 Allocation error.
+0x5 Error creating pixmap
+0x6 Cant create XImage
+0x7 Failed to read
+0x8 Warning: Failed to find valid format for alpha.
+0x9 Warning: Failed to create alpha picture.
+0xa Warning: Failed to create alpha pixmap.
+0xb Failed to find format for screen(%d)
+0xc Out of memory
+0xd Out of memory while allocating blue buffer.
+0xe Out of memory while allocating green buffer.
+0xf Out of memory while allocating red buffer.
+0x10 Failed to read theme item
+0x11 unsupported visual
+0x12 Setting default value
+
+$set 0x7 #Fluxbox
+
+0x1 rc filename is invalid!
+0x2 Failed to load groupfile
+0x3 Failed to load database
+0x4 Retrying with
+0x5 Warning! Could not find screen to map window on!
+0x6 Can't create %s directory
+0x7 Couldn't find screens to manage.\nMake sure you don't have another window manager running.
+0x8 Error parsing regular expression
+0x9 Fatal! There can only one instance of fluxbox class.
+0xa Can not connect to X server.\nMake sure you started X before you start Fluxbox.
+0xb Warning: X server does not support locale
+0xc Warning: cannot set locale modifiers
+
+$set 0x8 #Gnome
+
+0x1 Fatal: Out of memory, can't allocate for GNOME client list
+
+$set 0x9 #Keys
+
+0x1 Keys: Error on line
+0x2 Keys: Failed to merge keytree!
+0x3 Keys: Invalid key/modifier on line
+
+$set 0xa #Menu
+
+0x1 Configuration
+0x2 Fluxbox default menu
+0x3 Exit
+0x4 Icons
+0x5 Layer...
+0x6 On Head...
+0x7 Placement
+0x8 Reload Config
+0x9 Restart
+0xa xterm
+
+$set 0xb #Remember
+
+0x1 Decorations
+0x2 Dimensions
+0x3 Jump to workspace
+0x4 Layer
+0x5 Remember...
+0x6 Position
+0x7 Save on close
+0x8 Shaded
+0x9 Sticky
+0xa Unknown apps key
+0xb Workspace
+
+$set 0xc #Screen
+
+0x1 BScreen::BScreen: an error occured while querying the X server.\n	another window manager already running on display
+0x2 W: %4d x H: %4d
+0x3 W: 0000 x H: 0000
+0x4 BScreen::BScreen: managing screen %d using visual 0x%lx, depth %d\n
+0x5 X: %4d x Y: %4d
+0x6 0: 0000 x 0: 0000
+
+$set 0xd #Slit
+
+0x1 Clients
+0x2 Cycle Down
+0x3 Cycle Up
+0x4 Slit Direction
+0x5 Slit Layer
+0x6 Slit on Head
+0x7 Slit Placement
+0x8 Slit
+
+$set 0xe #Toolbar
+
+0x1 Edit current workspace name
+0x2 Iconbar Mode
+0x3 All Windows
+0x4 Icons
+0x5 None
+0x6 Workspace
+0x7 WorkspaceIcons
+0x8 Toolbar Layer
+0x9 Toolbar on Head
+0xa Toolbar Placement
+0xb Toolbar
+0xc Toolbar width percent
+
+$set 0xf #Window
+
+0x1 Unnamed
+
+$set 0x10 #Windowmenu
+
+0x1 Close
+0x2 Iconify
+0x3 Layer
+0x4 Lower
+0x5 Maximize
+0x6 Raise
+0x7 Send To ...
+0x8 Shade
+0x9 Stick
+
+$set 0x11 #Workspace
+
+0x1 Workspace %d
+0x2 Workspaces
+0x3 New Workspace
+0x4 Remove Last
+
+$set 0x12 #bsetroot
+
+0x1 %s: error: must specify one of: -solid, -mod, -gradient\n
+0x2 Couldn't create pixmap atoms, giving up!
+0x3 %s 2.0: (c) 1997-2000 Brad Hughes\n\n\
+-display <string>        display connection\n\
+-mod <x> <y>             modula pattern\n\
+-foreground, -fg <color> modula foreground color\n\
+-background, -bg <color> modula background color\n\n\
+-gradient <texture>      gradient texture\n\
+-from <color>            gradient start color\n\
+-to <color>              gradient end color\n\n\
+-solid <color>           solid color\n\n\
+-help                    print this help text and exit\n
+
+$set 0x13 #main
+
+0x1 error: '-display' requires an argument\n
+0x2 Bad Alloc
+0x3 Bad cast
+0x4 Out of range
+0x5 Runtime error
+0x6 Standard Exception
+0x7 Unknown error
+0x8 error: '-log' needs an argument
+0x9 Log File
+0xa Logging to
+0xb error: '-rc' requires an argument\n
+0xc Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
+-display <string>\t\tuse display connection.\n\
+-rc <string>\t\t\tuse alternate resource file.\n\
+-version\t\t\tdisplay version and exit.\n\
+-info\t\t\t\tdisplay some useful information.\n\
+-log <filename>\t\t\tlog output to file.\n\
+-help\t\t\t\tdisplay this help text and exit.\n\n
+
+$set 0xd #mainWarnDisplay
+
