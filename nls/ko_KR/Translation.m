901902330a638335e6ca5a2718073df73fe4ba4a
mathias <mathias>
2005-01-04 12:05:58 +0000

    updated ru_RU translations added ko_KR translations

---
+++ nls/ko_KR/Translation.m
@@ -0,0 +1,259 @@
+$ codeset=UTF-8
+
+$set 1 #Align
+
+1 하단 중앙
+2 하단 왼쪽
+3 하단 오른쪽
+4 [수평]
+5 왼쪽 정렬
+6 좌측 아래
+7 좌측 중간
+8 좌측 위
+9 양쪽 혼합
+10 오른쪽 정렬
+11 우측 아래
+12 우측 중간
+13 우측 위
+14 상단 중앙
+15 상단 왼쪽
+16 상단 오른쪽
+17 [수직]
+
+$set 2 #BaseDisplay
+
+1 Aborting... dumping core\n
+2 Shutting Down\n
+3 %s:      signal %d caught\n
+
+$set 3 #Common
+
+1 알파 채널값
+2 자동 숨김
+3 Compiled
+4 Compiled options
+5 Compiler
+6 Compiler version
+7 init
+8 keys
+9 menu
+10 style
+11 Defaults
+12 disabled
+13 Error
+14 Fluxbox version
+15 창 최대화 시 덮기
+16 툴바 표시
+
+$set 4 #Configmenu
+
+1 안티 앨리어스
+2 활성화된 창 자동 올림
+3 클릭 시 창 올림
+4 클릭 시 활성화
+5 Decorate Transient Windows
+6 마우스 휠로 작업공간 전환
+7 Focus Window on Workspace Change
+8 활성화 방법
+9 새로 여는 창 활성화
+10 모니터 전체 최대화
+11 이미지 디더링
+12 창 이동 시 내용 보임
+13 커서 위치 반자동 활성화
+14 커서 위치의 창 활성화
+15 순환식 작업공간 전환
+16 Force Pseudo-Transparency
+17 메뉴의 알파 채널값
+18 반투명 설정
+19 활성창의 알파 채널값
+20 비활성창의 알파 채널값
+
+$set 5 #Ewmh
+
+1 Fatal: Out of memory, can't allocate for EWMH client list
+
+$set 6 #FbTkError
+
+1 Warning! Height > 3200 setting Height = 3200
+2 Warning! Width > 3200 setting Width = 3200
+3 Warning: can't load fallback font
+4 Allocation error.
+5 Error creating pixmap
+6 Cant create XImage
+7 Failed to read
+8 Warning: Failed to find valid format for alpha.
+9 Warning: Failed to create alpha picture.
+10 Warning: Failed to create alpha pixmap.
+11 Failed to find format for screen(%d)
+12 Out of memory
+13 Out of memory while allocating blue buffer.
+14 Out of memory while allocating green buffer.
+15 Out of memory while allocating red buffer.
+16 Failed to read theme item
+17 unsupported visual
+18 Setting default value
+
+$set 7 #Fluxbox
+
+1 rc filename is invalid!
+2 Failed to load groupfile
+3 Failed to load database
+4 Retrying with
+5 Warning! Could not find screen to map window on!
+6 Can't create %s directory
+7 Couldn't find screens to manage.\nMake sure you don't have another window manager running.
+8 Error parsing regular expression
+9 Fatal! There can only one instance of fluxbox class.
+10 Can not connect to X server.\nMake sure you started X before you start Fluxbox.
+11 Warning: X server does not support locale
+12 Warning: cannot set locale modifiers
+
+$set 8 #Gnome
+
+1 Fatal: Out of memory, can't allocate for GNOME client list
+
+$set 9 #Keys
+
+1 Keys: Error on line
+2 Keys: Failed to merge keytree!
+3 Keys: Invalid key/modifier on line
+
+$set 10 #Menu
+
+1 설정
+2 Fluxbox default menu
+3 종료
+4 Icons
+5 레이어...
+6 On Head...
+7 위치
+8 Reload Config
+9 재시작
+10 xterm
+
+$set 11 #Remember
+
+1 테두리?
+2 크기
+3 해당 작업공간으로 점프
+4 레이어
+5 기억하기...
+6 위치
+7 종료 시의 값 기억
+8 셰이드?
+9 고정?
+10 Unknown apps key
+11 작업공간
+12 Head
+
+$set 12 #Screen
+
+1 BScreen::BScreen: an error occured while querying the X server.\n	another window manager already running on display
+2 W: %4d x H: %4d
+3 W: 0000 x H: 0000
+4 BScreen::BScreen: managing screen %d using visual 0x%lx, depth %d\n
+5 X: %4d x Y: %4d
+6 0: 0000 x 0: 0000
+
+$set 13 #Slit
+
+1 클라이언트
+2 한 칸씩 내려앉히기
+3 한 칸씩 밀어올리기
+4 Slit 방향
+5 Slit 레이어
+6 Slit on Head
+7 Slit 위치
+8 Slit
+9 SlitList 저장
+
+$set 14 #Toolbar
+
+1 작업공간 이름 변경
+2 아이콘바 모드
+3 모든 작업공간의 창 표시
+4 아이콘화 된 것만
+5 표시 안함
+6 현재 작업공간의 창 표시
+7 현재 작업공간에서 아이콘화 된 것만
+8 툴바 레이어
+9 Toolbar on Head
+10 툴바 위치
+11 툴바
+12 툴바 너비(%)
+13 시계: 24 시간제
+14 시계: 12 시간제
+15 시간 표시 형식 수정
+16 아이콘 그림 표시
+
+$set 15 #Window
+
+1 Unnamed
+
+$set 16 #Windowmenu
+
+1 닫기
+2 아이콘화
+3 레이어
+4 낮춤
+5 최대화
+6 올림
+7 Send To ...
+8 셰이드
+9 고정
+10 강제 종료
+
+$set 17 #Workspace
+
+1 작업공간 %d
+2 작업공간
+3 새 작업공간 추가
+4 마지막 것 제거
+
+$set 18 #bsetroot
+
+1 %s: error: must specify one of: -solid, -mod, -gradient\n
+2 Couldn't create pixmap atoms, giving up!
+3 %s 2.0: (c) 1997-2000 Brad Hughes\n\n\
+-display <string>        display connection\n\
+-mod <x> <y>             modula pattern\n\
+-foreground, -fg <color> modula foreground color\n\
+-background, -bg <color> modula background color\n\n\
+-gradient <texture>      gradient texture\n\
+-from <color>            gradient start color\n\
+-to <color>              gradient end color\n\n\
+-solid <color>           solid color\n\n\
+-help                    print this help text and exit\n
+
+$set 19 #main
+
+1 error: '-display' requires an argument\n
+2 Bad Alloc
+3 Bad cast
+4 Out of range
+5 Runtime error
+6 Standard Exception
+7 Unknown error
+8 error: '-log' needs an argument
+9 Log File
+10 Logging to
+11 error: '-rc' requires an argument\n
+12 Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
+-display <string>\t\tuse display connection.\n\
+-rc <string>\t\t\tuse alternate resource file.\n\
+-version\t\t\tdisplay version and exit.\n\
+-info\t\t\t\tdisplay some useful information.\n\
+-log <filename>\t\t\tlog output to file.\n\
+-help\t\t\t\tdisplay this help text and exit.\n\n
+13 warning: couldn't set environment variable 'DISPLAY'
+
+$set 20
+
+1 Dock 위
+2 하부
+3 데스크탑
+4 Dock
+5 표준
+6 상위
+
+
