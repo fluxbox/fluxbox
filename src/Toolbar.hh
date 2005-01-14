4f9370cafa693862cdd901ce8d237380900a95de
mathias <mathias>
2005-01-14 09:42:07 +0000

    copyright

--- src/Toolbar.hh
+++ src/Toolbar.hh
@@ -1,5 +1,5 @@
 // Toolbar.hh for Fluxbox
-// Copyright (c) 2002-2003 Henrik Kinnunen (fluxgen at users.sourceforge.net)
+// Copyright (c) 2002-2005 Henrik Kinnunen (fluxgen at users.sourceforge.net)
 //
 // Toolbar.hh for Blackbox - an X11 Window manager
 // Copyright (c) 1997 - 2000 Brad Hughes (bhughes@tcac.net)
