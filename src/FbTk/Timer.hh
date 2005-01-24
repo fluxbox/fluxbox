5e9814e0910318a022e979230140f86f97337aef
mathias <mathias>
2005-01-24 20:02:03 +0000

    fix to solve compileproblems under freebsd4

--- src/FbTk/Timer.hh
+++ src/FbTk/Timer.hh
@@ -42,9 +42,9 @@
 #include <inttypes.h>
 #endif // HAVE_INTTYPES_H
 
+#include <sys/types.h>
 #include <sys/select.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 namespace FbTk {
