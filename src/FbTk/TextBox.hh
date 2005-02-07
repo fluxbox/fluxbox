b82b3b98d2a86c6dde47170b1b94655fd733f9c1
mathias <mathias>
2005-02-07 23:39:57 +0000

    copyright year

--- src/FbTk/TextBox.hh
+++ src/FbTk/TextBox.hh
@@ -1,5 +1,5 @@
 // TextBox.hh for FbTk - fluxbox toolkit
-// Copyright (c) 2003 Henrik Kinnunen (fluxgen at fluxbox dot org)
+// Copyright (c) 2003 - 2005 Henrik Kinnunen (fluxgen at fluxbox dot org)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
