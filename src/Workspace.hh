acaf89bbb6efb2b0d0de43c2667fb2a4af8fb3a3
fluxgen <fluxgen>
2006-02-19 11:11:48 +0000

    moved placement strategies to different PlacementStrategy classes

--- src/Workspace.hh
+++ src/Workspace.hh
@@ -106,8 +106,7 @@ private:
 
     std::string m_name;  ///< name of this workspace
     unsigned int m_id;	///< id, obsolete, this should be in BScreen
-    int *m_cascade_x; ///< need a cascade for each head (Xinerama)
-    int *m_cascade_y; ///< need a cascade for each head (Xinerama)
+
 };
 
 
