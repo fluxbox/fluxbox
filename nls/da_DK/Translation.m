b366d9c498815a432f8fa96af64e54ee3c6e8849
simonb <simonb>
2006-03-05 01:28:36 +0000

    update Danish Translation Thanks to Jonas Koelker, jonaskoelker AT users.sourceforge.net

--- nls/da_DK/Translation.m
+++ nls/da_DK/Translation.m
@@ -4,65 +4,118 @@ $set 1 #Align
 
 1 Bunden i midten
 2 Bunden til venstre
-3 Til højre i bunden
+3 Bunden til højre
 4 Horisontal
-6 Left Bottom
-7 Left Center
-8 Left Top
-11 Right Bottom
-12 Right Center
-13 Right Top
+5 Til venstre
+6 Til venstre i bunden
+7 Til venstre i midten
+8 Til venstre i toppen
+9 Relativ
+10 Til højre
+11 Til højre i bunden
+12 Til højre i midten
+13 Til højre i toppen
 14 Toppen i midten
 15 Øverste venstre hjørne
-16 Øverst til højre
+16 Øverste højre hjørne
 17 Vertikal
 
 $set 2 #BaseDisplay
 
+1 Afslutter brat... laver et core dump\n
+2 Lukker ned\n
+3 %s:      signal %d fanget\n
 
 $set 3 #Common
 
+1 Alfa
 2 Gem automatisk
+10 menu
+11 stil
+13 Slået fra
+14 Fejl
+15 Fluxbox version
+17 SVN Revision
+18 Synlig
 
 $set 4 #Configmenu
 
 2 Hæv automatisk
+3 Klik hæver
 4 Klik for fokus
-7 Fokus vindue ved skrivebords ændring
-8 Fokus Model
-9 Fokus nyt vindue
-10 Fuld maximering
-11 Billede Dithering
-12 Uklar vinduesflytning
-13 Semi Sloppy Focus
+5 Dekorér transiente vinduer
+6 Skift skrivebord med scroll-hjulet
+7 Fokuser på vindue ved skrivebordsskift
+8 Fokusmodel
+9 Fokuser på nye vinduer
+10 Fuld maksimering
+11 Billede-dithering
+12 Ugennemsigtig flytning af vinduer
+13 Semisløset Focus
 14 Dovent fokus
 15 Workspace Warping
+16 Forcér pseudogennemsigtighed
+18 Gennemsigtighed
 
 $set 5 #Ewmh
 
+1 Fatal fejl: Løbet tør for hukommelse, ikke nok til at allokere EWMH's klientliste.
 
 $set 6 #FbTkError
 
+1 Advarsel! Højde > 3200, sætter højde = 3200
+2 Advarsel! Bredde > 3200, sætter bredde = 3200
+4 Allokeringsfejl.
+5 Fejl i oprettelse af pixmap
+7 Kunne ikke læse.
+12 Løbet tør for hukommelse
+13 Løbet tør for hukommelse under allokering af blå buffer.
+14 Løbet tør for hukommelse under allokering af grøn buffer.
+15 Løbet tør for hukommelse under allokering af rød buffer.
 
 $set 7 #Fluxbox
 
+1 rc filnavn ugyldigt.
+4 Kunne ikke indlæse databasen
+6 Kan ikke oprette mappen %s
+8 Fejl: kan ikke forstå det regulære udtryk
+9 Fatal fejl: der kan kun være en instans af klassen fluxbox.
+11 Advarsel: X-serveren understøtter ikke lokale indstillinger (locale)
 
 $set 8 #Gnome
 
+1 Fatal fejl: Løbet tør for hukommelse, ikke nok til at allokere GNOME's klientliste.
 
 $set 9 #Keys
 
+1 Taster (Keys): Fejl på linie
+2 Taster (Keys): kunne ikke sammenflette tastetræ!
+3 Taster (Keys): Ugyldig tast på linie
 
 $set 10 #Menu
 
+1 Indstillinger
+2 Fluxbox standardmenu
 3 Afslut
 4 Ikoner
+5 Lag...
 7 Placering
+8 Genindlæs indstillinger
 9 Genstart
 10 xterm
 
 $set 11 #Remember
 
+1 Dekorationer
+2 Dimensioner
+3 Gå til skriverbord
+4 Lag
+5 Husk...
+6 Placering
+7 Gem ved lukning
+8 Kun titelbjælke
+9 På alle skriveborde
+11 Skrivebord
 
 $set 12 #Screen
 
@@ -72,15 +125,28 @@ $set 12 #Screen
 
 $set 13 #Slit
 
-4 Slit retning
-7 Slit placering
-8 Slit
+1 Klienter
+4 Slidsretning
+5 Slidslag
+7 Slidsplacering
+8 Slids
+9 Gem slidsliste
 
 $set 14 #Toolbar
 
-1 Andring af skrivebordets navn
+1 Rediger skrivebordets navn
+3 Alle vinduer
+4 Ikoner
+5 Ingen
+6 Skrivebord
+7 Skrivebordsikoner
+8 Værktøjsbjælkelag
 10 Værktøjsbjælkens placering
 11 Værktøjsbjælke
+13 Ur: 24 timer
+14 Ur: 12 timer
+15 Rediger tidsformat
+16 Vis billeder
 
 $set 15 #Window
 
@@ -89,13 +155,15 @@ $set 15 #Window
 $set 16 #Windowmenu
 
 1 Luk
-2 Iconificeret
+2 Minimeret
+3 Lag
 4 Sænk
 5 Maksimer
 6 Hæv
 7 Send til ...
-8 Skygge
-9 Klæbrig
+8 Kun titelbjælke
+9 På alle skriveborde
+10 Luk abrupt
 
 $set 17 #Workspace
 
@@ -106,27 +174,39 @@ $set 17 #Workspace
 
 $set 18 #fbsetroot
 
-1 fejl: specificér en af følgende: -solid, -mod, -gradient\n
-3 -display <string>        skærmtilslutning\n\
+1 %s: fejl: specificér en af følgende: -solid, -mod, -gradient\n
+3 %s 2.0: (c) 1997-2000 Brad Hughes\n\n\
+-display <string>        skærmtilslutning\n\
 -mod <x> <y>             modula mønster\n\
 -foreground, -fg <color> modula foregrundsfarve\n\
 -background, -bg <color> modula baggrundsfarve\n\n\
--gradient <texture>      gradient tekstur\n\
--from <color>            gradient start farve\n\
--to <color>              gradient slut farve\n\n\
+-gradient <texture>      gradienttekstur\n\
+-from <color>            gradientens startfarve\n\
+-to <color>              gradientens slutfarve\n\n\
 -solid <color>           fast farve\n\n\
--help                    hvis denne hjælp og afslut\n
+-help                    vis denne hjælp og afslut\n
 
 $set 19 #main
 
-1 fjel: '-display' kræver et argument
-11 fejl: '-rc' kræver et argument
-13 Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
--display <string>\t\tbrug skærm tilslutning.\n\
--rc <string>\t\t\tbrug alternativ resource fil.\n\
+1 fejl: '-display' kræver et argument\n
+2 Dårlig allokering
+7 Ukendt fejl
+9 Logfil
+10 Logger til
+11 fejl: '-rc' kræver et argument\n
+12 Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
+-display <string>\t\tbrug skærmtilslutning.\n\
+-rc <string>\t\t\tbrug alternativ resourcefil.\n\
 -version\t\t\tvis versionsnummer og afslut.\n\
--info\t\t\t\tdisplay some useful information.\n\
--log <filename>\t\t\tlog output to file.\n\
+-info\t\t\t\tvis noget nyttig information.\n\
+-log <filename>\t\t\tlog output til fil.\n\
 -help\t\t\t\tvis denne hjælp og afslut.\n\n
 
+$set 20
 
+1 Over dok
+2 Nederst
+3 Skrivebord
+4 Dok
+5 Normal
+6 Øverst
