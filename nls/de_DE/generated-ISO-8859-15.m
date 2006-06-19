0fbe4c728d9be38fb63a980e9f88b0a51063ed4c
simonb <simonb>
2006-06-19 12:53:24 +0000

    update in line with Translation.m

--- nls/de_DE/generated-ISO-8859-15.m
+++ nls/de_DE/generated-ISO-8859-15.m
@@ -28,20 +28,20 @@ $set 2 #BaseDisplay
 
 $set 3 #Common
 
-1 Alpha Transparenz
-2 Automatisch Verstecken
-3 Compiler kompiliert am
+1 Alpha-Transparenz
+2 Automatisch verstecken
+3 Kompiliert am
 4 Einkompilierte Optionen
 5 Compiler
-6 Compiler Version
-7 Init Datei
+6 Compilerversion
+7 Init-Datei
 8 Tastendefinitionsdatei
 10 Menüdatei
 11 Stildatei (Theme)
-12 Standard Einstellungen
+12 Standardeinstellungen
 13 deaktiviert
 14 Fehler
-15 Fluxbox Version
+15 Fluxbox-Version
 16 Durch maximierte Fenster verdecken
 18 Sichtbar
 
@@ -49,8 +49,8 @@ $set 4 #Configmenu
 
 1 Kantenglättung (AntiAliasing)
 2 Automatisches Hervorheben
-3 Klicken zum Hervorheben
-4 Fokus durch Klick
+3 Klick holt in den Vordergrund
+4 Klick fokussiert
 5 Flüchtige (transient) Fenster dekorieren
 6 Desktop mit Mausrad wechseln
 7 Fenster nach Desktopwechsel fokussieren
@@ -58,40 +58,39 @@ $set 4 #Configmenu
 9 Neue Fenster fokussieren
 10 Vollständig Maximieren
 11 Bild-Dithering
-12 Fenster undurchsichtig bewegen
-13 Halb-"schlampiger" Fokus
-14 MausFokus
+12 Fensterinhalt beim Verschieben zeigen
+14 Mausfokus
 15 Fenster über Desktopgrenzen verschieben
 16 Erzwinge Pseudo-Transparenz
-17 Menü Alpha Transparenz
+17 Alpha-Transparenz für Menü
 18 Transparenz
-19 Alpha Transparenz fokussierter Fenster
-20 Alpha Transparenz nicht fokussierter Fenster
+19 Alpha-Transparenz fokussierter Fenster
+20 Alpha-Transparenz nicht fokussierter Fenster
 
 $set 5 #Ewmh
 
-1 Fataler Fehler: Kein Speicher mehr frei, EWMH client list kann nicht allokiert werden.
+1 Fataler Fehler: Kein Speicher mehr frei, kann keinen für EWMH-Clientliste zuweisen
 
 $set 6 #FbTkError
 
-1 Warnung! Höhe > 3200 setze Höhe = 3200
-2 Warnung! Breite > 3200 setze Breite = 3200
-3 Warnung: kann fallback Schriftart nicht laden
-4 Fehler beim Allokieren
+1 Warnung! Höhe > 3200, setze Höhe = 3200
+2 Warnung! Breite > 3200, setze Breite = 3200
+3 Warnung: Kann Fallback-Schriftart nicht laden
+4 Fehler beim Zuweisen
 5 Kann Pixmap nicht erzeugen
 6 Kann XImage nicht erzeugen
 7 Fehler beim Lesen
-8 Warnung: Kann kein gültiges Format für Alpha finden.
-9 Warnung: Kann Alpha Bild nicht erzeugen.
-10 Warnung: Kann Alpha Pixmap nicht erzeugen.
+8 Warnung: Kann kein gültiges Format für Alpha-Transparenz finden.
+9 Warnung: Kann Alpha-Bild nicht erzeugen.
+10 Warnung: Kann Alpha-Pixmap nicht erzeugen.
 11 Kann kein Format für screen(%d) finden
 12 Kein Speicher mehr frei
 13 Kein Speicher mehr frei für 'blue buffer'.
 14 Kein Speicher mehr frei für 'green buffer'.
 15 Kein Speicher mehr frei für 'red buffer'.
-16 Kann Stil (Theme) Eintrag nicht lesen
+16 Kann Stil- (Theme-) Eintrag nicht lesen
 17 Nicht unterstütztes visual
-18 Benutze Standard Einstellung
+18 Benutze Standardeinstellung
 
 $set 7 #Fluxbox
 
@@ -99,18 +98,18 @@ $set 7 #Fluxbox
 2 Kann groupfile nicht laden
 3 Kann database nicht laden
 4 Versuche es erneut mit
-5 Warnung! Kann keinen screen finden auf dem das Fenster dargestellt werden kann!
+5 Warnung! Kann keinen screen finden, um das Fenster darzustellen!
 6 Kann Verzeichnis %s nicht anlegen
-7 Kann keine screens finden.\nLäuft ein anderer Windowmanager bereits?
+7 Kann keine screens zur Verwaltung finden.\nLäuft bereits ein anderer Windowmanager?
 8 Fehler beim Parsen des regulären Ausdrucks
-9 Fataler Fehler! Es darf nur eine Instanz der Fluxbox Klasse geben
+9 Fataler Fehler! Es darf nur eine Instanz der Fluxbox-Klasse geben
 10 Kann nicht zum X-Server verbinden.\nX muss vor Fluxbox gestartet werden.
 11 Warnung: X-Server unterstützt Lokalisierung nicht
 12 Warnung: Kann Lokalisierung nicht setzen
 
 $set 8 #Gnome
 
-1 Fataler Fehler: Kein Speicher mehr frei, kann GNOME client list nicht allokieren
+1 Fataler Fehler: Kein Speicher mehr frei, kann keinen für GNOME-Clientlist zuweisen
 
 $set 9 #Keys
 
@@ -121,11 +120,11 @@ $set 9 #Keys
 $set 10 #Menu
 
 1 Konfiguration
-2 Fluxbox Standardmenü
+2 Fluxbox-Standardmenü
 3 Beenden
 4 Icons
 5 Ebene
-6 auf Monitor
+6 Auf Monitor
 7 Platzierung
 8 Konfiguration neu laden
 9 Neu starten
@@ -134,49 +133,50 @@ $set 11 #Remember
 
 1 Dekoration
 2 Dimension
-3 Gehe zum Desktop
+3 Gehe zu Desktop
 4 Ebene
 5 Einstellungen merken...
 6 Position
 7 Beim Schliessen speichern
 8 Aufgerollt
 9 Auf allen Desktops sichtbar
-10 Unbekannter apps Eintrag
-11 Momentanen Desktop
+10 Unbekannter apps-Eintrag
+11 Momentaner Desktop
 
 $set 12 #Screen
 
 1 BScreen::BScreen: Ein Fehler beim Verbinden zum X-Server ist aufgetreten.\nEin anderer Windowmanager läuft bereits auf dem Display 
-2 W: %4d x H: %4d
+2 B: %4d x H: %4d
 3 BScreen::BScreen: Verwalte screen %d mit visual 0x%lx, Farbtiefe %d\n
 
 $set 13 #Slit
 
-1 Aktive Slit Programme
-2 Eine Position runter bewegen
-3 Eine Position hoch bewegen
-4 Slit Ausrichtung
-5 Slit Ebene
+1 Aktive Slit-Programme
+2 Eine Position nach unten
+3 Eine Position nach oben
+4 Slit-Ausrichtung
+5 Ebene für Slit
 6 Slit auf Monitor
-7 Slit Platzierung
+7 Slit-Platzierung
 8 Slit
+9 SlitList speichern
 
 $set 14 #Toolbar
 
-1 Momentanen Desktopnamen bearbeiten
-2 Iconleiste
+1 Momentanen Desktopnamen ändern
+2 Iconleisten-Modus
 3 Alle Fenster
 4 Alle Fenster (nur minimierte)
 5 Keine
 6 Aktueller Desktop (alle Fenster)
 7 Aktueller Desktop (nur minimierte)
-8 Toolbar Ebene
+8 Ebene für Toolbar
 9 Toolbar auf Monitor
-10 Toolbar Platzierung
+10 Toolbar-Platzierung
 11 Toolbar
-12 Toolbar Grösse (Prozent)
-13 Uhr: 24 Stunden Format
-14 Uhr: 12 Stunden Format
+12 Toolbar-Größe (Prozent)
+13 Uhr: 24-Stunden-Format
+14 Uhr: 12-Stunden-Format
 15 Uhrzeitformat bearbeiten
 16 Programm-Icons anzeigen
 
@@ -188,13 +188,14 @@ $set 16 #Windowmenu
 
 1 Schliessen
 2 Minimieren
-3 Ebene ...
+3 Ebene
 4 Nach Hinten
 5 Maximieren
 6 Nach Vorne
-7 Senden an ...
+7 Verschieben nach ...
 8 Aufrollen
 9 Auf allen Desktops sichtbar
+10 Kill
 
 $set 17 #Workspace
 1 Desktop %d
@@ -204,17 +205,17 @@ $set 17 #Workspace
 
 $set 18 #fbsetroot
 
-1 error: must specify one of: -solid, -mod, -gradient\n
-2 Couldn't create pixmap atoms, giving up!
-3 -display <string>        display connection\n\
--mod <x> <y>             modula pattern\n\
--foreground, -fg <color> modula foreground color\n\
--background, -bg <color> modula background color\n\n\
--gradient <texture>      gradient texture\n\
--from <color>            gradient start color\n\
--to <color>              gradient end color\n\n\
--solid <color>           solid color\n\n\
--help                    print this help text and exit\n
+1 Fehler: Eins der Folgenden muss angegeben werden: -solid, -mod, -gradient\n
+2 Konnte Pixmap-Atome nicht erstellen, gebe auf!
+3 -display <string>        Verbindung anzeigen\n\
+-mod <x> <y>             Modula-Raster\n\
+-foreground, -fg <Farbe> Modula-Hintergrundfarbe\n\
+-background, -bg <Farbe> Modula-Vorderghrundfarbe\n\n\
+-gradient <Textur>       Farbverlauftextur\n\
+-from <Farbe>            Anfangsfarbe für Farbverlauf\n\
+-to <Farbe>              Endfarbe für Farbverlauf\n\n\
+-solid <Farbe>           Feste Farbe\n\n\
+-help                    Diese Hilfe anzeigen und beenden\n
 
 $set 19 #main
 
@@ -226,12 +227,13 @@ $set 19 #main
 6 Standard Exception
 7 Unbekannter Fehler
 8 Fehler: '-log' braucht ein Argument
-9 Log Datei
+9 Log-Datei
 10 Schreibe Log in
 11 Fehler: '-rc' braucht ein Argument
+12 Fehler: '-screen' braucht ein Argument
 13 Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
 -display <string>\t\tBenutze ein anderes $DISPLAY.\n\
--rc <string>\t\t\tBenutze andere Init-Datei.\n\
+-rc <string>\t\t\tBenutze eine andere Init-Datei.\n\
 -version\t\t\tZeige Version an.\n\
 -info\t\t\t\tZeige nützliche Informationen an.\n\
 -log <filename>\t\t\tSchreibe Log in Datei.\n\
@@ -244,3 +246,5 @@ $set 20 #LayerSet
 4 Dock
 5 Normal
 6 Oben
+
+
