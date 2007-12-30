fluxbox/fluxbox

commit b5c354b994bc06667abe35e2d528c0f025703c4e

architecture astronomy

src/Window.cc
@@ -264,8 +264,8 @@ FluxboxWindow::FluxboxWindow(WinClient &client, FbWinFrameTheme &tm,
     m_toggled_decos(false),
     m_icon_hidden(false),
     m_focus_hidden(false),
-    m_focus_new(screen().focusControl(), &FocusControl::focusNew),
-    m_mouse_focus(screen().focusControl(), &FocusControl::isMouseFocus),
+    m_focus_new(BoolAcc(screen().focusControl(), &FocusControl::focusNew)),
+    m_mouse_focus(BoolAcc(screen().focusControl(), &FocusControl::isMouseFocus)),
     m_click_focus(true),
     m_old_pos_x(0), m_old_pos_y(0),
     m_old_width(1),  m_old_height(1),
@@ -1745,17 +1745,18 @@ void FluxboxWindow::shade() {
 }
 
 void FluxboxWindow::shadeOn() {
-
     if (!shaded)
         shade();
-
 }
 
 void FluxboxWindow::shadeOff() {
-
     if (shaded)
         shade();
+}
 
+void FluxboxWindow::setShaded(bool val) {
+    if (val != shaded)
+        shade();
 }
 
 void FluxboxWindow::stick() {
@@ -1783,6 +1784,17 @@ void FluxboxWindow::stick() {
 
 }
 
+void FluxboxWindow::setStuck(bool val) {
+    if (val != stuck)
+        stick();
+}
+
+void FluxboxWindow::setIconic(bool val) {
+    if (val && isIconic())
+        deiconify();
+    if (!val && !isIconic())
+        iconify();
+}
 
 void FluxboxWindow::raise() {
     if (isIconic())

src/AlphaMenu.cc
@@ -25,23 +25,26 @@
 #include "AlphaMenu.hh"
 
 #include "Window.hh"
+#include "WindowCmd.hh"
 #include "Screen.hh"
 #include "Workspace.hh"
 #include "WindowCmd.hh"
 #include "fluxbox.hh"
 #include "Layer.hh"
-#include "IntResMenuItem.hh"
+#include "FbTk/IntMenuItem.hh"
 
 #include "FbTk/I18n.hh"
 #include "Window.hh"
-#include "WindowCmd.hh"
 
 AlphaMenu::AlphaMenu(MenuTheme &tm, FbTk::ImageControl &imgctrl,
-                     FbTk::XLayer &layer, AlphaObject &object):
-    ToggleMenu(tm, imgctrl, layer),
-    m_focused_alpha_resource(&object, &AlphaObject::getFocusedAlpha, &AlphaObject::setFocusedAlpha, 255),
-    m_unfocused_alpha_resource(&object, &AlphaObject::getUnfocusedAlpha, &AlphaObject::setUnfocusedAlpha, 255)
+                     FbTk::XLayer &layer):
+    ToggleMenu(tm, imgctrl, layer)
 {
+
+    static WindowAccessor<int> m_focused_alpha((WindowAccessor<int>::Getter)&FluxboxWindow::getFocusedAlpha,
+            (WindowAccessor<int>::Setter)&FluxboxWindow::setFocusedAlpha, 255);
+    static WindowAccessor<int> m_unfocused_alpha((WindowAccessor<int>::Getter)&FluxboxWindow::getUnfocusedAlpha,
+            (WindowAccessor<int>::Setter)&FluxboxWindow::setUnfocusedAlpha, 255);
 
     _FB_USES_NLS;
 
@@ -53,7 +56,7 @@ AlphaMenu::AlphaMenu(MenuTheme &tm, FbTk::ImageControl &imgctrl,
                   "Transparency level of the focused window");
 
     FbTk::MenuItem *focused_alpha_item =
-        new IntResMenuItem< ObjectResource<AlphaObject, int> >(focused_alpha_label, m_focused_alpha_resource, 0, 255, *this);
+        new FbTk::IntMenuItem(focused_alpha_label, m_focused_alpha, 0, 255, *this);
     insert(focused_alpha_item);
 
     const FbTk::FbString unfocused_alpha_label =
@@ -62,14 +65,14 @@ AlphaMenu::AlphaMenu(MenuTheme &tm, FbTk::ImageControl &imgctrl,
                   "Transparency level of unfocused windows");
 
     FbTk::MenuItem *unfocused_alpha_item =
-        new IntResMenuItem< ObjectResource<AlphaObject, int> >(unfocused_alpha_label, m_unfocused_alpha_resource, 0, 255, *this);
+        new FbTk::IntMenuItem(unfocused_alpha_label, m_unfocused_alpha, 0, 255, *this);
     insert(unfocused_alpha_item);
 
     const FbTk::FbString usedefault_label = _FB_XTEXT(Windowmenu, DefaultAlpha,
                                                       "Use Defaults",
                                                       "Default transparency settings for this window");
     FbTk::MenuItem *usedefault_item =
-        new AlphaMenuSelectItem(usedefault_label, &object, *this);
+        new AlphaMenuSelectItem(usedefault_label, *this);
     insert(usedefault_item);
 
     updateMenu();
@@ -81,8 +84,8 @@ void AlphaMenu::move(int x, int y) {
 
     if (isVisible()) {
         // TODO: hardcoding the indices is a bad idea
-        ((IntResMenuItem< ObjectResource<AlphaObject, int> >*)find(0))->updateLabel();
-        ((IntResMenuItem< ObjectResource<AlphaObject, int> >*)find(1))->updateLabel();
+        ((FbTk::IntMenuItem *)find(0))->updateLabel();
+        ((FbTk::IntMenuItem *)find(1))->updateLabel();
         frameWindow().updateBackground(false);
         FbTk::Menu::clearWindow();
     }
@@ -90,8 +93,8 @@ void AlphaMenu::move(int x, int y) {
     
 void AlphaMenu::show() {
     // TODO: hardcoding the indices is a bad idea
-    ((IntResMenuItem< ObjectResource<AlphaObject, int> >*)find(0))->updateLabel();
-    ((IntResMenuItem< ObjectResource<AlphaObject, int> >*)find(1))->updateLabel();
+    ((FbTk::IntMenuItem *)find(0))->updateLabel();
+    ((FbTk::IntMenuItem *)find(1))->updateLabel();
     frameWindow().updateBackground(false);
     FbTk::Menu::clearWindow();
 

src/AlphaMenu.hh
@@ -26,56 +26,40 @@
 #define ALPHAMENU_HH
 
 #include "ToggleMenu.hh"
+#include "WindowCmd.hh"
 #include "FbTk/MenuItem.hh"
-#include "ObjectResource.hh"
-
-class AlphaObject {
-public:
-
-    virtual int getFocusedAlpha() const = 0;
-    virtual int getUnfocusedAlpha() const = 0;
-    virtual bool getUseDefaultAlpha() const = 0;
-
-    virtual void setFocusedAlpha(int alpha) = 0;
-    virtual void setUnfocusedAlpha(int alpha) = 0;
-    virtual void setDefaultAlpha() = 0;
-
-    virtual ~AlphaObject() {};
-};
-
 
 class AlphaMenu : public ToggleMenu {
 public:
     AlphaMenu(MenuTheme &tm, FbTk::ImageControl &imgctrl,
-              FbTk::XLayer &layer, AlphaObject &object);
+              FbTk::XLayer &layer);
 
     // we override these to update the menu when the active window changes
     void move(int x, int y);
     void show();
-
-    ObjectResource<AlphaObject, int> m_focused_alpha_resource;
-    ObjectResource<AlphaObject, int> m_unfocused_alpha_resource;
-
 };
 
 class AlphaMenuSelectItem : public FbTk::MenuItem {
 
 public:
-    AlphaMenuSelectItem(const FbTk::FbString &label, AlphaObject *object, AlphaMenu &parent):
-        FbTk::MenuItem(label), m_object(object), m_parent(parent) {
+    AlphaMenuSelectItem(const FbTk::FbString &label, AlphaMenu &parent):
+        FbTk::MenuItem(label), m_parent(parent) {
         setToggleItem(true);
         setCloseOnClick(false);
     }
 
-    bool isSelected() const { return m_object->getUseDefaultAlpha(); }
+    bool isSelected() const {
+        static ConstWindowAccessor<bool> s_is_default(&FluxboxWindow::getUseDefaultAlpha, true);
+        return s_is_default;
+    }
     void click(int button, int time, unsigned int mods) {
-        m_object->setDefaultAlpha();
+        static WindowCmd<void> s_set_default(&FluxboxWindow::setDefaultAlpha);
+        s_set_default.execute();
         m_parent.show(); // cheat to refreshing the window
         FbTk::MenuItem::click(button, time, mods);
     }
 
 private:
-    AlphaObject *m_object;
     AlphaMenu &m_parent;
 };
 

src/FbTk/Accessor.hh
@@ -1,0 +1,85 @@
+// Accessor.hh
+// Copyright (c) 2007 Fluxbox Team (fluxgen at fluxbox dot org)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#ifndef FBTK_ACCESSOR_HH
+#define FBTK_ACCESSOR_HH
+
+namespace FbTk {
+
+// base class for objects that act like data type T
+template <typename T>
+class Accessor {
+public:
+    virtual Accessor<T> &operator =(const T &val) = 0;
+    virtual operator T() const = 0;
+};
+
+// essentially just a reference
+template <typename T>
+class SimpleAccessor: public Accessor<T> {
+public:
+    SimpleAccessor(T &val): m_val(val) { }
+    inline Accessor<T> &operator =(const T &val) { m_val = val; return *this; }
+    inline operator T() const { return m_val; }
+
+private:
+    T &m_val;
+};
+
+// use object methods as an accessor
+template <typename T, typename Receiver>
+class ObjectAccessor: public Accessor<T> {
+public:
+    typedef T (Receiver:: *Getter)() const;
+    typedef void (Receiver:: *Setter)(T &);
+    ObjectAccessor(Receiver &r, Getter g, Setter s):
+        m_receiver(r), m_getter(g), m_setter(s) { }
+
+    inline operator T() const { return (m_receiver.*m_getter)(); }
+    inline Accessor<T> &operator =(const T &val) {
+        (m_receiver.*m_setter)(val); return *this;
+    }
+
+private:
+    Receiver &m_receiver;
+    Getter m_getter;
+    Setter m_setter;
+};
+
+// same as above but with no set method
+template <typename T, typename Receiver>
+class ConstObjectAccessor: public Accessor<T> {
+public:
+    typedef T (Receiver:: *Getter)() const;
+    ConstObjectAccessor(const Receiver &r, Getter g):
+        m_receiver(r), m_getter(g) { }
+
+    inline operator T() const { return (m_receiver.*m_getter)(); }
+    inline Accessor<T> &operator =(const T &val) { return *this; }
+
+private:
+    const Receiver &m_receiver;
+    Getter m_getter;
+};
+
+}; // end namespace FbTk
+
+#endif // FBTK_ACCESSOR_HH

src/FbTk/BoolMenuItem.hh
@@ -1,5 +1,5 @@
-// BoolMenuItem.hh for Fluxbox Window Manager
-// Copyright (c) 2003 - 2006 Henrik Kinnunen (fluxgen at fluxbox dot org)
+// BoolMenuItem.hh for FbTk
+// Copyright (c) 2003 - 2007 Henrik Kinnunen (fluxgen at fluxbox dot org)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -19,24 +19,25 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// $Id$
-
-#ifndef BOOLMENUITEM_HH
-#define BOOLMENUITEM_HH
+#ifndef FBTK_BOOLMENUITEM_HH
+#define FBTK_BOOLMENUITEM_HH
 
 #include "MenuItem.hh"
+#include "Accessor.hh"
+
+namespace FbTk {
 
 /// a bool menu item
 class BoolMenuItem: public FbTk::MenuItem {
 public:
-    BoolMenuItem(const FbTk::FbString &label, bool &item, 
+    BoolMenuItem(const FbTk::FbString &label, Accessor<bool> &item, 
                  FbTk::RefCount<FbTk::Command> &cmd):
         FbTk::MenuItem(label, cmd), m_item(item) { 
         FbTk::MenuItem::setSelected(m_item);
         setToggleItem(true);
         setCloseOnClick(false);
     }
-    BoolMenuItem(const FbTk::FbString &label, bool &item):
+    BoolMenuItem(const FbTk::FbString &label, Accessor<bool> &item):
         FbTk::MenuItem(label), m_item(item) {
         FbTk::MenuItem::setSelected(m_item);
         setToggleItem(true);
@@ -53,38 +54,9 @@ public:
         FbTk::MenuItem::setSelected(m_item);
     }
 private:
-    bool &m_item;
+    Accessor<bool> &m_item;
 };
 
-/// a bool menu item
-template <typename Type>
-class BoolResMenuItem: public FbTk::MenuItem {
-public:
-    BoolResMenuItem(const FbTk::FbString &label, Type &res, 
-                 FbTk::RefCount<FbTk::Command> &cmd):
-        FbTk::MenuItem(label, cmd), m_res(res) { 
-        FbTk::MenuItem::setSelected(*m_res);
-        setToggleItem(true);
-        setCloseOnClick(false);
-    }
-    BoolResMenuItem(const FbTk::FbString &label, Type &res):
-        FbTk::MenuItem(label), m_res(res) {
-        FbTk::MenuItem::setSelected(*m_res);
-        setToggleItem(true);
-        setCloseOnClick(false);
-    }
-    bool isSelected() const { return *m_res; }
-    // toggle state
-    void click(int button, int time, unsigned int mods) {
-        setSelected(!*m_res);
-        FbTk::MenuItem::click(button, time, mods);
-    }
-    void setSelected(bool value) { 
-        m_res = value;
-        FbTk::MenuItem::setSelected(*m_res);
-    }
-private:
-    Type &m_res;
-};
+}; // end namespace FbTk
 
-#endif // BOOLMENUITEM_HH
+#endif // FBTK_BOOLMENUITEM_HH

src/FbTk/DefaultValue.hh
@@ -22,58 +22,32 @@
 #ifndef FBTK_DEFAULTVALUE_HH
 #define FBTK_DEFAULTVALUE_HH
 
+#include "Accessor.hh"
+
 namespace FbTk {
 
-// classes for overriding default values without having to listen for changes
-template <typename T>
-class DefaultValue {
+// class for overriding default values and restoring them later
+
+// Ret = type of value that gets returned
+// Def = type of default value -- may be Accessor<Ret> &, for example
+template <typename Ret, typename Def=Ret &>
+class DefaultValue: public Accessor<Ret> {
 public:
-    DefaultValue(const T &def):
+    DefaultValue(const Def def):
         m_default(def), m_actual(def), m_use_default(true) { }
 
-    inline const T &get() const { return m_use_default ? m_default : m_actual; }
-    inline void set(const T &val) { m_use_default = false; m_actual = val; }
     inline void restoreDefault() { m_use_default = true; }
     inline bool isDefault() const { return m_use_default; }
 
-    inline DefaultValue<T> &operator =(const T &val) {
-        set(val); return *this;
+    inline DefaultValue<Ret, Def> &operator =(const Ret &val) {
+        m_use_default = false; m_actual = val; return *this;
     }
 
-    inline operator T() const { return get(); }
+    inline operator Ret() const { return m_use_default ? m_default : m_actual; }
 
 private:
-    const T &m_default;
-    T m_actual;
-    bool m_use_default;
-};
-
-// designed for use with built-in types T, thus no need to return references
-template <typename T, typename Receiver>
-class DefaultAccessor {
-public:
-    typedef T (Receiver:: *Accessor)() const;
-    DefaultAccessor(const Receiver &r, Accessor a):
-        m_receiver(r), m_accessor(a), m_actual((r.*a)()),
-        m_use_default(true) { }
-
-    inline const T get() const {
-        return m_use_default ? (m_receiver.*m_accessor)() : m_actual;
-    }
-    inline void set(const T &val) { m_use_default = false; m_actual = val; }
-    inline void restoreDefault() { m_use_default = true; }
-    inline bool isDefault() const { return m_use_default; }
-
-    inline DefaultAccessor<T, Receiver> &operator =(const T &val) {
-        set(val); return *this;
-    }
-
-    inline operator T() const { return get(); }
-
-private:
-    const Receiver &m_receiver;
-    Accessor m_accessor;
-    T m_actual;
+    const Def m_default;
+    Ret m_actual;
     bool m_use_default;
 };
 

src/FbTk/IntMenuItem.hh
@@ -1,5 +1,5 @@
-// IntResMenuItem.hh for Fluxbox Window Manager
-// Copyright (c) 2003 Henrik Kinnunen (fluxgen at fluxbox dot org)
+// IntMenuItem.hh for FbTk
+// Copyright (c) 2003-2007 Henrik Kinnunen (fluxgen at fluxbox dot org)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -19,21 +19,21 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-// $Id$
-
-#ifndef INTRESMENUITEM_HH
-#define INTRESMENUITEM_HH
+#ifndef FBTK_INTMENUITEM_HH
+#define FBTK_INTMENUITEM_HH
 
 #include "MenuItem.hh"
-#include "Resource.hh"
+#include "Accessor.hh"
 
 #include <string>
 
+namespace FbTk {
+
 /// Changes an resource integer value between min and max
-template <typename Type>
-class IntResMenuItem: public FbTk::MenuItem {
+class IntMenuItem: public FbTk::MenuItem {
 public:
-    IntResMenuItem(const FbTk::FbString &label, Type &res, int min_val, int max_val, FbTk::Menu &host_menu) :
+    IntMenuItem(const FbTk::FbString &label, Accessor<int> &res,
+                   int min_val, int max_val, FbTk::Menu &host_menu) :
         FbTk::MenuItem(label, host_menu), m_org_label(FbTk::MenuItem::label()),
         m_max(max_val), m_min(min_val), m_res(res) { 
         updateLabel();
@@ -61,16 +61,16 @@ public:
 
         // make sure values stay within bounds _before_ we try to set m_res
         // otherwise, this may cause bugs (say, with casting to unsigned char)
-        if ((button == 4 || button == 3) && *m_res < m_max) { // up
-            if (*m_res + inc_val < m_max)
-                m_res.get() += inc_val;
+        if ((button == 4 || button == 3) && m_res < m_max) { // up
+            if (m_res + inc_val < m_max)
+                m_res = m_res + inc_val;
             else
-                m_res.get() = m_max;
-        } else if ((button == 5 || button == 1) && *m_res > m_min) { // down
-            if (*m_res - inc_val >= m_min)
-                m_res.get() -= inc_val;
+                m_res = m_max;
+        } else if ((button == 5 || button == 1) && m_res > m_min) { // down
+            if (m_res - inc_val >= m_min)
+                m_res = m_res - inc_val;
             else
-                m_res.get() = m_min;
+                m_res = m_min;
         }
 
         // update label
@@ -87,14 +87,16 @@ public:
     }
 
     void updateLabel() {
-        setLabel(appendIntValue(m_org_label, *m_res));
+        setLabel(appendIntValue(m_org_label, m_res));
     }
 
 private:
     std::string m_org_label; ///< original label
     const int m_max; ///< maximum value the integer can have
     const int m_min; ///< minimum value the integer can have
-    Type &m_res; ///< resource item to be changed
+    Accessor<int> &m_res; ///< resource item to be changed
 };
 
-#endif // INTRESMENUITEM_HH
+}; // end namespace FbTk
+
+#endif // FBTK_INTMENUITEM_HH

src/FbTk/Makefile.am
@@ -16,7 +16,7 @@ imlib2_SOURCE= ImageImlib2.hh ImageImlib2.cc
 endif
 
 libFbTk_a_SOURCES = App.hh App.cc Color.cc Color.hh Command.hh \
-	ObjectRegistry.hh DefaultValue.hh \
+	ObjectRegistry.hh Accessor.hh DefaultValue.hh \
 	FileUtil.hh FileUtil.cc \
 	EventHandler.hh EventManager.hh EventManager.cc \
 	FbWindow.hh FbWindow.cc Font.cc Font.hh FontImp.hh \
@@ -25,6 +25,7 @@ libFbTk_a_SOURCES = App.hh App.cc Color.cc Color.hh Command.hh \
 	LogicCommands.hh LogicCommands.cc \
 	MacroCommand.hh MacroCommand.cc \
 	Menu.hh Menu.cc MenuItem.hh MenuItem.cc \
+	BoolMenuItem.hh IntMenuItem.hh \
 	MultiButtonMenuItem.hh MultiButtonMenuItem.cc \
 	MenuTheme.hh MenuTheme.cc NotCopyable.hh \
 	RefCount.hh SimpleCommand.hh SignalHandler.cc SignalHandler.hh \

commit f4ee09779b9d9d3d0ddfb293de84308538be281a

Fix minimize regression caused by b5c354b9.

src/Window.cc
@@ -1790,9 +1790,9 @@ void FluxboxWindow::setStuck(bool val) {
 }
 
 void FluxboxWindow::setIconic(bool val) {
-    if (val && isIconic())
+    if (!val && isIconic())
         deiconify();
-    if (!val && !isIconic())
+    if (val && !isIconic())
         iconify();
 }
 