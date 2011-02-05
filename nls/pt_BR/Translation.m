4614c186c8c3502f5677e23f2ace23b851276776
Sergio Cipolla <secipolla at gmail.com>
2011-02-05 19:32:50 +0100

    updated pt_BR translations, closes #2940952

--- nls/pt_BR/Translation.m
+++ nls/pt_BR/Translation.m
@@ -1,270 +1,281 @@
-$ codeset=ISO-8859-1
+$ codeset=UTF-8
 
 $set 1 #Align
 
-1 Em Baixo no Centro
-2 Em Baixo a Esquerda
-3 Em Baixo a Direita
+1 Embaixo no centro
+2 Embaixo √† esquerda
+3 Embaixo √† direita
 4 Horizontal
-5 Esquerda
-6 Esquerda em Baixo
-7 Esquerda no Centro
-8 Esquerda em Cima
+5 √Ä esquerda
+6 √Ä esquerda embaixo
+7 √Ä esquerda no centro
+8 √Ä esquerda em cima
 9 Relativo
-10 Direita
-11 Direita em Baixo
-12 Direita no Centro
-13 Direita em Cima
-14 Em Cima no Centro
-15 Em Cima a Esquerda
-16 Em Cima a Direita
+10 √Ä direita
+11 √Ä direita embaixo
+12 √Ä direita no centro
+13 √Ä direita em cima
+14 Em cima no centro
+15 Em cima √† esquerda
+16 Em cima √† direita
 17 Vertical
+18 No centro
+19 Em cima
+20 Embaixo
 
 $set 2 #BaseDisplay
 
-1 Abortando... despejando core\n
+1 Abortando... despejando o n√∫cleo\n
 2 Desligando\n
 3 %s:      sinal %d recebido\n
 
 $set 3 #Common
 
-1 TransparÍncia
-2 Esconder Automaticamente
+1 Alfa
+2 Auto-ocultar
 3 Compilado
-4 OpÁıes de CompilaÁ„o
+4 Op√ß√µes de compila√ß√£o
 5 Compilador
-6 Vers„o do Compilador
+6 Vers√£o do compilador
 7     init
 8     keys
 9      nls
 10     menu
 11    style
-12 Padrıes
+12 Padr√µes
 13 desativado
 14 Erro
-15 Vers„o do Fluxbox
-16 Maximizar Sobre
-17 Revis„o GIT
-18 VisÌvel
-19 N„o existe opÁ„o de Papel de Parede neste tema.\nConsulte o manual ou leia o FAQ.
+15 Vers√£o do Fluxbox
+16 Maximizar sobre
+17 Revis√£o do GIT
+18 Vis√≠vel
+19 N√£o h√° uma op√ß√£o de fundo especificada neste tema.\nPor favor, consulte o manual ou leia o FAQ.
 
 $set 4 #Configmenu
 
-1 AntiAlias
-2 Auto Sobrepor
-3 Clicar Sobrepıe Janela
-4 Foco na Janela Quando Clicada
-5 DecoraÁ„o em Janelas Tempor·rias
-6 Roda do Mouse muda ¡rea de Trabalho
-7 Foco na ⁄ltima Janela Da ¡rea de Trabalho
-8 Modelo de Foco
-9 Foco em Novas Janelas
-10 MaximizaÁ„o Total
-11 SimulaÁ„o de Cores em Imagens
-12 Movimento Opaco de Janelas
-14 Foco com Mouse
-15 ¡reas de Trabalho ContÌnuas
-16 ForÁar Pseudo-TransparÍncia
-17 TransparÍncia do Menu
-18 TransparÍncia
-19 TransparÍncia de Janelas com Foco
-20 TransparÍncia de Janelas sem Foco
-21 OpÁıes de Abas
-22 Abas na Barra de TÌtulo
-23 Dimens„o de Abas Externas
-24 Foco com Click na Aba
-25 Foco com Mouse na Aba
+1 Antialias
+2 Por cima automaticamente
+3 Clicar p√µe por cima
+4 Clicar para focar
+5 Decorar janelas transit√≥rias
+6 Roda do mouse sobre o desktop muda espa√ßo de trabalho
+7 Focar a janela ao mudar espa√ßo de trabalho
+8 Modelo de foco
+9 Focar novas janelas
+10 Maximiza√ß√£o total
+11 Ditheriza√ß√£o de imagens
+12 Movimenta√ß√£o opaca de janelas
+14 Foco com o mouse
+15 Espa√ßos de trabalho cont√≠nuos
+16 For√ßar pseudotranspar√™ncia
+17 Alfa do Menu
+18 Transpar√™ncia
+19 Alfa da janela com foco
+20 Alfa da janela sem foco
+21 Op√ß√µes de abas
+22 Abas na barra de t√≠tulo
+23 Largura da aba externa
+24 Clicar foca a aba
+25 Mouse foca a aba
+26 Op√ß√µes de maximiza√ß√£o
+27 Ignorar incremento do redimensionamento
+28 Desabilitar a movimenta√ß√£o
+29 Desabilitar o redimensionamento
 
 $set 5 #Ewmh
 
-1 Fatal: Falta de memÛria, n„o È possÌvel alocar lista de clientes para EWMH
+1 Fatal: mem√≥ria insuficiente, n√£o √© poss√≠vel alocar mem√≥ria para a lista de clientes EWMH
 
 $set 6 #FbTkError
 
-1 Aviso! Altura > 3200, definindo Altura = 3200
-2 Aviso! Largura > 3200, definindo Largura = 3200
-3 Aviso: n„o foi possÌvel abrir fonte de recurso
-4 Erro de alocaÁ„o.
-5 Erro na criaÁ„o de pixmap
-6 N„o È possÌvel criar XImage
-7 Falha de leitura
-8 Aviso: N„o foi possÌvel encontrar um formato v·lido para transparÍncia.
-9 Aviso: N„o foi possÌvel criar uma imagem transparente.
-10 Aviso: N„o foi possÌvel criar um pixmap transparente.
-11 N„o foi possÌvel encontrar o formato da tela(%d)
-12 Falta de memÛria
-13 Falta de memÛria ao alocar buffer de azul.
-14 Falta de memÛria ao alocar buffer de verde.
-15 Falta de memÛria ao alocar buffer de vermelho.
-16 Falha de leitura de item de tema
-17 visual n„o suportado
-18 Definindo valor padr„o
+1 Aten√ß√£o! Altura > 3200, definindo Altura = 3200
+2 Aten√ß√£o! Largura > 3200, definindo Largura = 3200
+3 Aviso: n√£o √© poss√≠vel carregar a fonte de reserva
+4 Erro de aloca√ß√£o.
+5 Erro na cria√ß√£o de pixmap
+6 N√£o √© poss√≠vel criar XImage
+7 Falha na leitura
+8 Aviso: n√£o foi poss√≠vel encontrar um formato v√°lido para o alfa.
+9 Aviso: n√£o foi poss√≠vel criar uma imagem alfa.
+10 Aviso: n√£o foi poss√≠vel criar um pixmap alfa.
+11 N√£o foi poss√≠vel encontrar um formato para a tela(%d)
+12 Mem√≥ria insuficiente
+13 Mem√≥ria insuficiente ao alocar o buffer do azul.
+14 Mem√≥ria insuficiente ao alocar o buffer do verde.
+15 Mem√≥ria insuficiente ao alocar o buffer do vermelho.
+16 Falha na leitura de item do tema
+17 visual n√£o suportado
+18 Definindo pelo valor padr√£o
 
 $set 7 #Fluxbox
 
-1 Arquivo de configuraÁ„o È inv·lido!
-2 N„o foi possÌvel abrir arquivo de grupos
-3 N„o foi possÌvel abrir a base de dados
-4 Tentar de novo com
-5 Aviso! N„o foi encontrado uma tela onde mapear a janela!
-6 N„o È possÌvel criar diretorio %s
-7 N„o foram encontrados telas para gerenciar.\nAssegure-se de que n„o exite outro gerenciador de janelas ativo.
-8 Erro no tratamento de express„o regular
-9 Fatal! SÛ È possÌvel haver uma inst‚ncia da classe fluxbox.
-10 N„o È possÌvel conectar ao servidor X.\nAssegure-se de que X foi iniciado antes do Fluxbox.
-11 Aviso: Servidor X n„o suporta locale
-12 Aviso: N„o È possÌvel definir modificadores de locale
+1 O nome do arquivo rc √© inv√°lido!
+2 N√£o foi poss√≠vel carregar o arquivo de grupos
+3 N√£o foi poss√≠vel carregar a base de dados
+4 Tentando novamente com
+5 Aten√ß√£o! N√£o foi encontrada uma tela para onde mapear a janela!
+6 N√£o √© poss√≠vel criar diretorio %s
+7 N√£o foram encontradas telas para gerenciar.\nAssegure-se de que n√£o h√° outro gerenciador de janelas ativo.
+8 Erro ao analisar express√£o regular
+9 Fatal! S√≥ √© poss√≠vel haver uma inst√¢ncia da classe fluxbox.
+10 N√£o √© poss√≠vel conectar-se ao servidor X.\nAssegure-se de que o X foi iniciado antes do Fluxbox.
+11 Aviso: o servidor X n√£o suporta o locale
+12 Aviso: n√£o √© poss√≠vel estabelecer modificadores de locale
 
 $set 8 #Gnome
 
-1 Fatal: Sem memÛria, n„o È possÌvel alocar lista de clientes para GNOME
+1 Fatal: mem√≥ria insuficiente, n√£o √© poss√≠vel alocar mem√≥ria para a lista de clientes do GNOME
 
 $set 9 #Keys
 
-1 Teclas: Erro na linha
-2 Teclas: Falha ao fundir ‡rvore de atalhos de teclas!
-3 Teclas: Tecla/Modificador inv·lido na linha
+1 Keys: erro na linha
+2 Keys: n√£o foi poss√≠vel fundir a √°rvore de teclas!
+3 Keys: tecla/modificador inv√°lido na linha
 
 $set 10 #Menu
 
-1 ConfiguraÁ„o
-2 Menu padr„o do Fluxbox
+1 Configura√ß√£o
+2 Menu padr√£o do Fluxbox
 3 Sair
-4 Õcones
+4 Janelas minimizadas
 5 Camada...
-6 No Monitor...
+6 No monitor...
 7 Posicionamento
-8 Recarregar ConfiguraÁıes
+8 Recarregar a configura√ß√£o
 9 Reiniciar
-10 Aviso: definiÁ„o [encondig] n„o-balanceada
+10 Aviso: etiquetas [codifica√ß√£o] desbalanceadas
 
 $set 11 #Remember
 
-1 DecoraÁıes
-2 Dimensıes
-3 Ir para ¡rea de Trabalho
+1 Decora√ß√µes
+2 Dimens√µes
+3 Ir para o espa√ßo de trabalho
 4 Camada
-5 Lembrar...
-6 PosiÁ„o
-7 Gravar ao sair
-8 Enrolado
-9 Sempre VisÌvel
-10 Tecla de AplicaÁ„o Desconhecido
-11 ¡rea de Trabalho
+5 Memorizar...
+6 Posi√ß√£o
+7 Salvar ao sair
+8 Enrolada
+9 Pregada
+10 Chave apps desconhecida
+11 Espa√ßo de trabalho
 12 Monitor
-13 TransparÍncia
+13 Transpar√™ncia
 
 $set 12 #Screen
 
-1 BScreen::BScreen: ocorreu um erro ao interrogar o servidor X.\n outro gerenciador de janelas est· ativo
+1 BScreen::BScreen: ocorreu um erro ao interrogar o servidor X.\n outro gerenciador de janelas est√° ativo
 2 L: %4d x A: %4d
-3 BScreen::BScreen: gerenciando tela %d usando visual 0x%lx, profundidade %d\n
+3 BScreen::BScreen: gerenciando tela %d usando visual 0x%lx, com profundidade %d\n
 4 L: %04d x A: %04d
 
 $set 13 #Slit
 
 1 Clientes
-2 Rodar para Baixo
-3 Rodar para Cima
-4 DirecÁ„o
-5 Camada
-6 Monitor
-7 Posicionamento
+2 Circular para baixo
+3 Circular para cima
+4 Dire√ß√£o do Slit:
+5 Camada do Slit
+6 Slit no monitor
+7 Posicionamento do Slit
 8 Slit
-9 Gravar Slitlist
+9 Salvar lista do Slit
 
 $set 14 #Toolbar
 
-1 Editar o nome da ¡rea de Trabalho
-2 Modo da Barra de Janelas
-3 Todas as Janelas
-4 Icones
+1 Renomear o espa√ßo de trabalho atual
+2 Modo da barra de janelas
+3 Todas as janelas
+4 Minimizadas
 5 Nenhuma
-6 ¡rea de Trabalho
-7 Icones da ¡rea de Trabalho
-8 Camada da Barra de Ferramentas
-9 Barra de Ferramentas no Monitor
-10 Posicionamento
-11 Barra de Ferramentas
-12 Largura em percentagem
-13 RelÛgio: 24h
-14 RelÛgio: 12h
-15 Editar Formato do RelÛgio
-16 Mostrar Imagens
-17 Sem-Icones
-18 ¡rea de Trabalho Sem-Icones
+6 Espa√ßo de trabalho
+7 Minimizadas no espa√ßo de trabalho
+8 Camada da barra de tarefas
+9 Barra de tarefas no monitor
+10 Posicionamento da barra de tarefas
+11 Barra de tarefas
+12 Largura da barra em porcentagem
+13 Rel√≥gio: 24h
+14 Rel√≥gio: 12h
+15 Editar o formato do rel√≥gio
+16 Exibir imagens
+17 N√£o minimizadas
+18 N√£o minimizadas no espa√ßo de trabalho
 
 $set 15 #Window
 
-1 Sem Nome
+1 Sem nome
 
 $set 16 #Windowmenu
 
 1 Fechar
 2 Minimizar
 3 Camada
-4 Desfocalizar
+4 Por baixo
 5 Maximizar
-6 Focalizar
-7 Enviar Para ...
+6 Por cima
+7 Enviar para...
 8 Enrolar
-9 Sempre VisÌvel
+9 Pregar
 10 Matar
-11 Usar Valores Padr„o
+11 Usar valores padr√£o
+12 Definir o t√≠tulo da janela
 
 $set 17 #Workspace
 
-1 ¡rea de Trabalho %d
-2 ¡reas de Trabalho
-3 Nova ¡rea de Trabalho
-4 Remover ⁄ltima ¡rea de Trabalho
+1 Espa√ßo de trabalho %d
+2 Espa√ßos de trabalho
+3 Novo espa√ßo de trabalho
+4 Remover o √∫ltimo
 
 $set 18 #fbsetroot
 
-1 Erro: È necess·rio especificar ao menos um dos seguintes argumentos: -solid, -mod, -gradient\n
-2 N„o foi possÌvel criar ·tomos de pixmaps, desistindo!
-3 -display <string>        conex„o com display\n\
--mod <x> <y>             altera padr„o\n\
--foreground, -fg <color> altera cor de primeiro plano\n\
--background, -bg <color> altera cor de fundo\n\n\
--gradient <texture>      textura do gradiente\n\
--from <color>            cor de inÌcio do gradiente\n\
--to <color>              cor do fim do gradiente\n\n\
--solid <color>           cor sÛlida\n\n\
--help                    mostra esta ajuda e sai\n
+1 Erro: √© necess√°rio especificar um entre: -solid, -mod, -gradient\n
+2 N√£o foi poss√≠vel criar √°tomos de pixmap, desistindo!
+3 -display <string>        conex√£o com monitor\n\
+-mod <x> <y>             padr√£o modula\n\
+-foreground, -fg <cor>   cor de primeiro plano modula\n\
+-background, -bg <cor>   cor de fundo modula\n\n\
+-gradient <textura>      textura do gradiente\n\
+-from <cor>              cor de in√≠cio do gradiente\n\
+-to <cor>                cor de t√©rmino do gradiente\n\n\
+-solid <cor>             cor s√≥lida\n\n\
+-help                    exibir esta ajuda e sair\n
 
 $set 19 #main
 
 1 erro: '-display' requer um argumento
-2 Erro de AlocaÁ„o
-3 Erro de Convers„o
-4 Fora dos Limites
-5 Erro de ExecuÁ„o
-6 ExcepÁ„o Standard
+2 Aloca√ß√£o inv√°lida
+3 Convers√£o inv√°lida
+4 Fora dos limites
+5 Erro de execu√ß√£o
+6 Exce√ß√£o padr√£o
 7 Erro desconhecido
-8 Erro: '-log' necessita um argumento
-9 Arquivo de Log
-10 Guardando Log em
-11 erro: '-rc' necessita de um argumento
-12 erro: '-screen' necessita de um argumento
+8 erro: '-log' requer um argumento
+9 Arquivo de log
+10 Registrando o log em
+11 erro: '-rc' requer um argumento
+12 erro: '-screen' requer um argumento
 13 Fluxbox %s: (c) %s Henrik Kinnunen\n\
-Website: http://www.fluxbox.org/\n\n\
--display <string>\t\tusar conex„o com o display.\n\
--screen <all|int,int,int>\tativar apenas nas telas especificadas.\n\
--rc <string>\t\t\tusar arquivo alternativo de recursos.\n\
--version\t\t\tmostrar vers„o e sair.\n\
--info\t\t\t\tmostrar algumas informaÁıes ˙teis.\n\
--log <filename>\t\t\tguarda log em um arquivo.\n\
--help\t\t\t\tmostrar esta ajuda e sair.\n\n
-14 aviso: n„o foi possÌvel definir a vari·vel de ambiente 'DISPLAY'
+Site: http://www.fluxbox.org/\n\n\
+-display <string>\t\tusar conex√£o com o monitor.\n\
+-screen <all|int,int,int>\texecutar apenas nas telas especificadas.\n\
+-rc <string>\t\t\tusar arquivo de configura√ß√£o alternativo.\n\
+-version\t\t\texibir a vers√£o e finalizar.\n\
+-info\t\t\t\texibir algumas informa√ß√µes √∫teis.\n\
+-list-commands\t\t\tlistar todos os comandos de teclas v√°lidos.\n\
+-sync\t\t\t\tsincronizar com o servidor X para depura√ß√£o.\n\
+-log <nomedoarquivo>\t\t\tlogar a sa√≠da no arquivo.\n\
+-help\t\t\t\texibir esta ajuda e sair.\n\n
+14 aviso: n√£o foi poss√≠vel definir a vari√°vel de ambiente 'DISPLAY'
 
 $set 20 #layers
 
-1 Acima do Dock
-2 Em Baixo
+1 Acima do dock
+2 Embaixo
 3 Desktop
 4 Dock
 5 Normal
-6 Topo
+6 Em cima
+
 
 
--- nls/pt_BR/generated-UTF-8.m
+++ nls/pt_BR/generated-UTF-8.m
@@ -2,38 +2,41 @@ $ codeset=UTF-8
 
 $set 1 #Align
 
-1 Em Baixo no Centro
-2 Em Baixo a Esquerda
-3 Em Baixo a Direita
+1 Embaixo no centro
+2 Embaixo à esquerda
+3 Embaixo à direita
 4 Horizontal
-5 Esquerda
-6 Esquerda em Baixo
-7 Esquerda no Centro
-8 Esquerda em Cima
+5 À esquerda
+6 À esquerda embaixo
+7 À esquerda no centro
+8 À esquerda em cima
 9 Relativo
-10 Direita
-11 Direita em Baixo
-12 Direita no Centro
-13 Direita em Cima
-14 Em Cima no Centro
-15 Em Cima a Esquerda
-16 Em Cima a Direita
+10 À direita
+11 À direita embaixo
+12 À direita no centro
+13 À direita em cima
+14 Em cima no centro
+15 Em cima à esquerda
+16 Em cima à direita
 17 Vertical
+18 No centro
+19 Em cima
+20 Embaixo
 
 $set 2 #BaseDisplay
 
-1 Abortando... despejando core\n
+1 Abortando... despejando o núcleo\n
 2 Desligando\n
 3 %s:      sinal %d recebido\n
 
 $set 3 #Common
 
-1 Transparência
-2 Esconder Automaticamente
+1 Alfa
+2 Auto-ocultar
 3 Compilado
-4 Opções de Compilação
+4 Opções de compilação
 5 Compilador
-6 Versão do Compilador
+6 Versão do compilador
 7     init
 8     keys
 9      nls
@@ -43,114 +46,118 @@ $set 3 #Common
 13 desativado
 14 Erro
 15 Versão do Fluxbox
-16 Maximizar Sobre
-17 Revisão GIT
+16 Maximizar sobre
+17 Revisão do GIT
 18 Visível
-19 Não existe opção de Papel de Parede neste tema.\nConsulte o manual ou leia o FAQ.
+19 Não há uma opção de fundo especificada neste tema.\nPor favor, consulte o manual ou leia o FAQ.
 
 $set 4 #Configmenu
 
-1 AntiAlias
-2 Auto Sobrepor
-3 Clicar Sobrepõe Janela
-4 Foco na Janela Quando Clicada
-5 Decoração em Janelas Temporárias
-6 Roda do Mouse muda Área de Trabalho
-7 Foco na Última Janela Da Área de Trabalho
-8 Modelo de Foco
-9 Foco em Novas Janelas
-10 Maximização Total
-11 Simulação de Cores em Imagens
-12 Movimento Opaco de Janelas
-14 Foco com Mouse
-15 Áreas de Trabalho Contínuas
-16 Forçar Pseudo-Transparência
-17 Transparência do Menu
+1 Antialias
+2 Por cima automaticamente
+3 Clicar põe por cima
+4 Clicar para focar
+5 Decorar janelas transitórias
+6 Roda do mouse sobre o desktop muda espaço de trabalho
+7 Focar a janela ao mudar espaço de trabalho
+8 Modelo de foco
+9 Focar novas janelas
+10 Maximização total
+11 Ditherização de imagens
+12 Movimentação opaca de janelas
+14 Foco com o mouse
+15 Espaços de trabalho contínuos
+16 Forçar pseudotransparência
+17 Alfa do Menu
 18 Transparência
-19 Transparência de Janelas com Foco
-20 Transparência de Janelas sem Foco
-21 Opções de Abas
-22 Abas na Barra de Título
-23 Dimensão de Abas Externas
-24 Foco com Click na Aba
-25 Foco com Mouse na Aba
+19 Alfa da janela com foco
+20 Alfa da janela sem foco
+21 Opções de abas
+22 Abas na barra de título
+23 Largura da aba externa
+24 Clicar foca a aba
+25 Mouse foca a aba
+26 Opções de maximização
+27 Ignorar incremento do redimensionamento
+28 Desabilitar a movimentação
+29 Desabilitar o redimensionamento
 
 $set 5 #Ewmh
 
-1 Fatal: Falta de memória, não é possível alocar lista de clientes para EWMH
+1 Fatal: memória insuficiente, não é possível alocar memória para a lista de clientes EWMH
 
 $set 6 #FbTkError
 
-1 Aviso! Altura > 3200, definindo Altura = 3200
-2 Aviso! Largura > 3200, definindo Largura = 3200
-3 Aviso: não foi possível abrir fonte de recurso
+1 Atenção! Altura > 3200, definindo Altura = 3200
+2 Atenção! Largura > 3200, definindo Largura = 3200
+3 Aviso: não é possível carregar a fonte de reserva
 4 Erro de alocação.
 5 Erro na criação de pixmap
 6 Não é possível criar XImage
-7 Falha de leitura
-8 Aviso: Não foi possível encontrar um formato válido para transparência.
-9 Aviso: Não foi possível criar uma imagem transparente.
-10 Aviso: Não foi possível criar um pixmap transparente.
-11 Não foi possível encontrar o formato da tela(%d)
-12 Falta de memória
-13 Falta de memória ao alocar buffer de azul.
-14 Falta de memória ao alocar buffer de verde.
-15 Falta de memória ao alocar buffer de vermelho.
-16 Falha de leitura de item de tema
+7 Falha na leitura
+8 Aviso: não foi possível encontrar um formato válido para o alfa.
+9 Aviso: não foi possível criar uma imagem alfa.
+10 Aviso: não foi possível criar um pixmap alfa.
+11 Não foi possível encontrar um formato para a tela(%d)
+12 Memória insuficiente
+13 Memória insuficiente ao alocar o buffer do azul.
+14 Memória insuficiente ao alocar o buffer do verde.
+15 Memória insuficiente ao alocar o buffer do vermelho.
+16 Falha na leitura de item do tema
 17 visual não suportado
-18 Definindo valor padrão
+18 Definindo pelo valor padrão
 
 $set 7 #Fluxbox
 
-1 Arquivo de configuração é inválido!
-2 Não foi possível abrir arquivo de grupos
-3 Não foi possível abrir a base de dados
-4 Tentar de novo com
-5 Aviso! Não foi encontrado uma tela onde mapear a janela!
+1 O nome do arquivo rc é inválido!
+2 Não foi possível carregar o arquivo de grupos
+3 Não foi possível carregar a base de dados
+4 Tentando novamente com
+5 Atenção! Não foi encontrada uma tela para onde mapear a janela!
 6 Não é possível criar diretorio %s
-7 Não foram encontrados telas para gerenciar.\nAssegure-se de que não exite outro gerenciador de janelas ativo.
-8 Erro no tratamento de expressão regular
+7 Não foram encontradas telas para gerenciar.\nAssegure-se de que não há outro gerenciador de janelas ativo.
+8 Erro ao analisar expressão regular
 9 Fatal! Só é possível haver uma instância da classe fluxbox.
-10 Não é possível conectar ao servidor X.\nAssegure-se de que X foi iniciado antes do Fluxbox.
-11 Aviso: Servidor X não suporta locale
-12 Aviso: Não é possível definir modificadores de locale
+10 Não é possível conectar-se ao servidor X.\nAssegure-se de que o X foi iniciado antes do Fluxbox.
+11 Aviso: o servidor X não suporta o locale
+12 Aviso: não é possível estabelecer modificadores de locale
 
 $set 8 #Gnome
 
-1 Fatal: Sem memória, não é possível alocar lista de clientes para GNOME
+1 Fatal: memória insuficiente, não é possível alocar memória para a lista de clientes do GNOME
 
 $set 9 #Keys
 
-1 Teclas: Erro na linha
-2 Teclas: Falha ao fundir àrvore de atalhos de teclas!
-3 Teclas: Tecla/Modificador inválido na linha
+1 Keys: erro na linha
+2 Keys: não foi possível fundir a árvore de teclas!
+3 Keys: tecla/modificador inválido na linha
 
 $set 10 #Menu
 
 1 Configuração
 2 Menu padrão do Fluxbox
 3 Sair
-4 Ícones
+4 Janelas minimizadas
 5 Camada...
-6 No Monitor...
+6 No monitor...
 7 Posicionamento
-8 Recarregar Configurações
+8 Recarregar a configuração
 9 Reiniciar
-10 Aviso: definição [encondig] não-balanceada
+10 Aviso: etiquetas [codificação] desbalanceadas
 
 $set 11 #Remember
 
 1 Decorações
 2 Dimensões
-3 Ir para Área de Trabalho
+3 Ir para o espaço de trabalho
 4 Camada
-5 Lembrar...
+5 Memorizar...
 6 Posição
-7 Gravar ao sair
-8 Enrolado
-9 Sempre Visível
-10 Tecla de Aplicação Desconhecido
-11 Área de Trabalho
+7 Salvar ao sair
+8 Enrolada
+9 Pregada
+10 Chave apps desconhecida
+11 Espaço de trabalho
 12 Monitor
 13 Transparência
 
@@ -158,113 +165,117 @@ $set 12 #Screen
 
 1 BScreen::BScreen: ocorreu um erro ao interrogar o servidor X.\n outro gerenciador de janelas está ativo
 2 L: %4d x A: %4d
-3 BScreen::BScreen: gerenciando tela %d usando visual 0x%lx, profundidade %d\n
+3 BScreen::BScreen: gerenciando tela %d usando visual 0x%lx, com profundidade %d\n
 4 L: %04d x A: %04d
 
 $set 13 #Slit
 
 1 Clientes
-2 Rodar para Baixo
-3 Rodar para Cima
-4 Direcção
-5 Camada
-6 Monitor
-7 Posicionamento
+2 Circular para baixo
+3 Circular para cima
+4 Direção do Slit:
+5 Camada do Slit
+6 Slit no monitor
+7 Posicionamento do Slit
 8 Slit
-9 Gravar Slitlist
+9 Salvar lista do Slit
 
 $set 14 #Toolbar
 
-1 Editar o nome da Área de Trabalho
-2 Modo da Barra de Janelas
-3 Todas as Janelas
-4 Icones
+1 Renomear o espaço de trabalho atual
+2 Modo da barra de janelas
+3 Todas as janelas
+4 Minimizadas
 5 Nenhuma
-6 Área de Trabalho
-7 Icones da Área de Trabalho
-8 Camada da Barra de Ferramentas
-9 Barra de Ferramentas no Monitor
-10 Posicionamento
-11 Barra de Ferramentas
-12 Largura em percentagem
+6 Espaço de trabalho
+7 Minimizadas no espaço de trabalho
+8 Camada da barra de tarefas
+9 Barra de tarefas no monitor
+10 Posicionamento da barra de tarefas
+11 Barra de tarefas
+12 Largura da barra em porcentagem
 13 Relógio: 24h
 14 Relógio: 12h
-15 Editar Formato do Relógio
-16 Mostrar Imagens
-17 Sem-Icones
-18 Área de Trabalho Sem-Icones
+15 Editar o formato do relógio
+16 Exibir imagens
+17 Não minimizadas
+18 Não minimizadas no espaço de trabalho
 
 $set 15 #Window
 
-1 Sem Nome
+1 Sem nome
 
 $set 16 #Windowmenu
 
 1 Fechar
 2 Minimizar
 3 Camada
-4 Desfocalizar
+4 Por baixo
 5 Maximizar
-6 Focalizar
-7 Enviar Para ...
+6 Por cima
+7 Enviar para...
 8 Enrolar
-9 Sempre Visível
+9 Pregar
 10 Matar
-11 Usar Valores Padrão
+11 Usar valores padrão
+12 Definir o título da janela
 
 $set 17 #Workspace
 
-1 Área de Trabalho %d
-2 Áreas de Trabalho
-3 Nova Área de Trabalho
-4 Remover Última Área de Trabalho
+1 Espaço de trabalho %d
+2 Espaços de trabalho
+3 Novo espaço de trabalho
+4 Remover o último
 
 $set 18 #fbsetroot
 
-1 Erro: é necessário especificar ao menos um dos seguintes argumentos: -solid, -mod, -gradient\n
-2 Não foi possível criar átomos de pixmaps, desistindo!
-3 -display <string>        conexão com display\n\
--mod <x> <y>             altera padrão\n\
--foreground, -fg <color> altera cor de primeiro plano\n\
--background, -bg <color> altera cor de fundo\n\n\
--gradient <texture>      textura do gradiente\n\
--from <color>            cor de início do gradiente\n\
--to <color>              cor do fim do gradiente\n\n\
--solid <color>           cor sólida\n\n\
--help                    mostra esta ajuda e sai\n
+1 Erro: é necessário especificar um entre: -solid, -mod, -gradient\n
+2 Não foi possível criar átomos de pixmap, desistindo!
+3 -display <string>        conexão com monitor\n\
+-mod <x> <y>             padrão modula\n\
+-foreground, -fg <cor>   cor de primeiro plano modula\n\
+-background, -bg <cor>   cor de fundo modula\n\n\
+-gradient <textura>      textura do gradiente\n\
+-from <cor>              cor de início do gradiente\n\
+-to <cor>                cor de término do gradiente\n\n\
+-solid <cor>             cor sólida\n\n\
+-help                    exibir esta ajuda e sair\n
 
 $set 19 #main
 
 1 erro: '-display' requer um argumento
-2 Erro de Alocação
-3 Erro de Conversão
-4 Fora dos Limites
-5 Erro de Execução
-6 Excepção Standard
+2 Alocação inválida
+3 Conversão inválida
+4 Fora dos limites
+5 Erro de execução
+6 Exceção padrão
 7 Erro desconhecido
-8 Erro: '-log' necessita um argumento
-9 Arquivo de Log
-10 Guardando Log em
-11 erro: '-rc' necessita de um argumento
-12 erro: '-screen' necessita de um argumento
+8 erro: '-log' requer um argumento
+9 Arquivo de log
+10 Registrando o log em
+11 erro: '-rc' requer um argumento
+12 erro: '-screen' requer um argumento
 13 Fluxbox %s: (c) %s Henrik Kinnunen\n\
-Website: http://www.fluxbox.org/\n\n\
--display <string>\t\tusar conexão com o display.\n\
--screen <all|int,int,int>\tativar apenas nas telas especificadas.\n\
--rc <string>\t\t\tusar arquivo alternativo de recursos.\n\
--version\t\t\tmostrar versão e sair.\n\
--info\t\t\t\tmostrar algumas informações úteis.\n\
--log <filename>\t\t\tguarda log em um arquivo.\n\
--help\t\t\t\tmostrar esta ajuda e sair.\n\n
+Site: http://www.fluxbox.org/\n\n\
+-display <string>\t\tusar conexão com o monitor.\n\
+-screen <all|int,int,int>\texecutar apenas nas telas especificadas.\n\
+-rc <string>\t\t\tusar arquivo de configuração alternativo.\n\
+-version\t\t\texibir a versão e finalizar.\n\
+-info\t\t\t\texibir algumas informações úteis.\n\
+-list-commands\t\t\tlistar todos os comandos de teclas válidos.\n\
+-sync\t\t\t\tsincronizar com o servidor X para depuração.\n\
+-log <nomedoarquivo>\t\t\tlogar a saída no arquivo.\n\
+-help\t\t\t\texibir esta ajuda e sair.\n\n
 14 aviso: não foi possível definir a variável de ambiente 'DISPLAY'
 
 $set 20 #layers
 
-1 Acima do Dock
-2 Em Baixo
+1 Acima do dock
+2 Embaixo
 3 Desktop
 4 Dock
 5 Normal
-6 Topo
+6 Em cima
+
 
 
