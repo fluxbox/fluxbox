fluxbox/fluxbox

commit 1f0adef4daa2da5b08ed7f41e7a0ce1e3f71e46f

better use of API, no need for 'timeval' in Menu

src/FbTk/Menu.cc
@@ -1200,10 +1200,7 @@ void Menu::closeMenu() {
 }
 
 void Menu::startHide() {
-    timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = theme()->getDelay() * 1000; // transformed to usec
-    m_hide_timer.setTimeout(timeout);
+    m_hide_timer.setTimeout(theme()->getDelay() * 1000);
     m_hide_timer.start();
 }
 

commit d2e7feaa855352986ff78cc9b2017bcebb78ea6c

bugfix: submenus didn't hide if a delay was set
use the FbTk::Timer API correctly, bug(s) introduced by

   1f0adef4daa2da5b08ed7f41e7a0ce1e3f71e46f
   e68511794130388ab9668fdef0dcf48dbbf002fd

src/AttentionNoticeHandler.cc
@@ -82,7 +82,7 @@ void AttentionNoticeHandler::addAttention(Focusable &client) {
     RefCount<Command<void> > cmd(new ToggleFrameFocusCmd(client));
     Timer *timer = new Timer();
     timer->setCommand(cmd);
-    timer->setTimeout(0, **timeout_res * 1000);
+    timer->setTimeout(**timeout_res);
     timer->fireOnce(false); // will repeat until window has focus
     timer->start();
 

src/FbTk/Menu.cc
@@ -1002,7 +1002,7 @@ void Menu::motionNotifyEvent(XMotionEvent &me) {
         }
 
         if (itmp->submenu()) { // start submenu open delay
-            m_submenu_timer.setTimeout(0, theme()->getDelay() * 1000);
+            m_submenu_timer.setTimeout(theme()->getDelay());
             m_submenu_timer.start();
         } else if (isItemSelectable(w)){
             // else normal menu item
@@ -1221,7 +1221,7 @@ void Menu::closeMenu() {
 }
 
 void Menu::startHide() {
-    m_hide_timer.setTimeout(theme()->getDelay() * 1000);
+    m_hide_timer.setTimeout(theme()->getDelay());
     m_hide_timer.start();
 }
 