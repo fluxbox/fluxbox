2790b996f3f2ef2d122aa093cbeb6aa00e626bf2
Mesutcan Kurt <mesutcank at gmail dot com>
2011-03-28 09:47:20 +0200

    updated turkish translations

--- nls/tr_TR/Translation.m
+++ nls/tr_TR/Translation.m
@@ -2,83 +2,204 @@ $ codeset=UTF-8
 
 $set 1 #Align
 
-1 Alt orta
-2 Sol alt
-3 Sağ üst
+1 Alt Orta
+2 Alt Sol
+3 Alt Sağ
 4 Yatay
-6 Orta sol
-7 Sol orta
-8 Sol üst
-11 Sağ orta
-12 Orta sağ
+5 Sol
+6 Sol Alt
+7 Sol Orta
+8 Sol Üst
+9 Bağlı
+10 Sağ
+11 Sağ Alt
+12 Sağ Orta
 13 Sağ üst
-14 Üst orta
-15 Sol üst
-16 Sağ üst
+14 Üst Orta
+15 Üst Sol
+16 Üst Sağ
 17 Dikey
+18 Orta
+19 Üst
+20 Alt
 
 $set 2 #BaseDisplay
 
+1 İptal Ediliyor... çekirdek dökümü alınıyor\n
+2 Kapatılıyor\n
+3 %s:      %d sinyali alındı\n
 
 $set 3 #Common
 
+1 Alfa
 2 Otomatik Gizle
+3 Derlenmiş
+4 Derlenmiş ayarlar
+5 Derleyici
+6 Derleyici versiyonu
+7     başlat
+8     tuşlar
+9      nls
+10     menü
+11    stil
+12 Varsayılanlar
+13 Devre Dışı
+14 Hata
+15 Fluxbox versiyonu
+16 En üst seviyeye çıkart
+17 GIT Revizyonu
+18 Görünür
+19 Bu stil için arkaplan seçeneği yok.\nLütfen kılavuzu ya da SSS'yi okuyun.
 
 $set 4 #Configmenu
 
-2 Otomatik Göster
-4 Hizalamak için tıkla
-7 Masaüstü değişiminde pencereyi hizala
-8 Hizalama
-9 Yeni Pencereleri Hizala
-10 Ekranı Kapla
-11 Resim oluşturulması
-12 Donuk Pencere Taşınması
-13 Aynıları Hizala
-14 Yavaş Hizala
-15 Masaüstü Sapması
+1 Kenar Düzleştirme
+2 Otomatik Büyütme
+3 Tıklayarak Büyütme
+4 Odaklamak için Tıklayın
+5 Geçici Pencereleri Süsler
+6 Masaüstleri Arası Fare Tekerleği ile Geçiş
+7 Çalışma Alanı Değiştiğinde Pencereye Odaklan
+8 Kalıba Odakla
+9 Yeni Pencereye Odakla
+10 Tamamen büyütme
+11 Resim titretme
+12 Opak Pencere Taşıma
+14 Fare Odağı
+15 Çalışma Alanı Sapması
+16 Sahte Transparanlık için Zorla
+17 Alfa Menü
+18 Transparanlık
+19 Odaklaılmamış Alfa Pencere
+20 Odaklanılmamış Alfa Pencere
+21 Sekme ÖzellikleriTab Options
+22 Başlık Çubuğunda Sekmeler
+23 Harici Sekme Genişliği
+24 Tıklama Sekmesi Odağı
+25 Fare Sekmesi Odağı
+26 Ayarları En Üst Düzeye Getir
+27 Yeniden Boyutlandırma Arttırmasını Görmezden Gel
+28 Taşımayı Devre Dışı Bırak
+29 Yeniden Boyutlandırmayı Devre Dışı Bırak
 
 $set 5 #Ewmh
 
+1 Ölümcül: Bellek Yetersiz, EWMH istemci listesi için ayrılamıyor
 
 $set 6 #FbTkError
 
+1 Uyarı! Boy > 3200, Boy 3200 olarak ayarlanıyor
+2 Uyarı! En > 3200, En 3200 olarak ayarlanıyor
+3 Uyarı! Son çare fontu yüklenemiyor
+4 Ayırma Hatası
+5 Pixmap oluştururken Hata
+6 XImage oluşturulamıyor
+7 Okunamıyor
+8 Uyarı: Alfa için uygun biçim bulunamadı.
+9 Uyarı: Alfa resmi oluşturulamadı.
+10 Uyarı: Alfa pixmap'ı oluşturulamadı.
+11 (%d) ekranı için biçim bulunamadı
+12 Bellek yetersiz
+13 Mavi tampon için bellek ayrılırken bellek yetersiz.
+14 Yeşil tampon için bellek yarılırken bellek yetersiz.
+15 Kırmızı tampon için bellek ayrılırken bellek yetersiz.
+16 Tema öğesi okunamadı
+17 Desteklenmeyen görsel
+18 Varsayılan değer ayarlanıyor
 
 $set 7 #Fluxbox
 
+1 rc dosya adı geçersiz!
+2 group dosyası yüklenemedi
+3 Veritabanı yüklenemedi
+4 Tekrar deneniyor:
+5 Uyarı! Pencereleri eşleyecek ekran bulunamıyor!
+6 %s dizini oluşturulamıyor
+7 Düzenlenecek pencereler bulunamıyor.\nBaşka bir penceere yöneticisinin çalışmadığından emin olun.
+8 Düzenli ifadeler (regular expression) ayrıştırılırken hata
+9 Ölümcül! Fluxbox sınıflarından sadece bir örnek olabilir.
+10 X sunucusuna bağlanılamıyor.\nFluxbox'u başlatmadan önce X'i başlattığınızdan emin olun.
+11 Uyarı: X sunucusu yerelleştirmeyi desteklemiyor
+12 Uyarı: Yerelleştirme tanımlayıcıları ayarlanamıyor
 
 $set 8 #Gnome
 
+1 Ölümcül: Bellek yetersiz, GNOME istemci listesi için yer ayrılamıyor
 
 $set 9 #Keys
 
+1 Tuşlar: Satırda hata
+2 Tuşlar: Tuş ağacı birleştirilirken hata!
+3 Tuşlar: Satırda geçersiz tuş/tanımlayıcı
 
 $set 10 #Menu
 
-3 Çık
-4 İkonlar
-7 Yerleşim
-9 Yeniden Başlat
+1 Yapılandırma
+2 Fluxbox varsayılan menü
+3 Çıkış
+4 Simgeler
+5 Katman...
+6 En başta...
+7 Yerleştirme
+8 Ayarları yeniden yükle
+9 Yeniden başlat
+10 Uyarı: dengesiz [çözme] imler
 
 $set 11 #Remember
 
+1 Süslemeler
+2 Boyutlar
+3 Çalışma alanına atlanıyor
+4 Katman
+5 Hatırla...
+6 Pozisyon
+7 Çıkışta kaydet
+8 Gölgelenmiş
+9 Yapışkan
+10 Bilinmeyen uygulama tuşu
+11 Çalışma Alanı
+12 Baş
+13 Transparanlık
 
 $set 12 #Screen
 
-2 Y: %4d x E: %4d
-4 Y: %04d x E: %04d
+1 BScreen::BScreen: X sunucusu sorgulanırken hata oluştu.\n	şu an ekranda diğer pencere yöneticisi çalışıyor
+2 E: %4d x B: %4d
+3 BScreen::BScreen: %d ekranı düzenleniyor 0x%lx görseli, %d derinliği kullanılıyor\n
+4 E: %04d x B: %04d
 
 $set 13 #Slit
 
-4 Slit yönü
-7 Slit yerleşimi
-8 Slit
+1 İstemciler
+2 Devir aşağı
+3 Devir yukarı
+4 Kesit Yönü:
+5 Kesit Katmanı
+6 Kesit tepede
+7 Kesit Yerleşimi
+8 Kesit
+9 Kesit listesini kaydet
 
 $set 14 #Toolbar
 
-1 Masaüstü ismini değiştir
-10 Araç Çubuğu Yerleşimi
-11 Araç Çubuğu
+1 Şu anki çalışma alanını yeniden adlandır
+2 Simge çubuğu modu
+3 Bütün pencelerler
+4 İkonlar
+5 Hiç (None)
+6 Çalışma alanı
+7 Çalışma alanı simgeleri
+8 Araç çubuğu Katmanı
+9 Araç çubuğu en üstte
+10 Araç çubuğu yerleştirmesi
+11 Araç çubuğu
+12 Araç çubuğu genişlik yüzdesi
+13 Saat: 24s
+14 Saat: 12s
+15 Saat Biçimini Düzenle
+16 Resimleri Göster
+17 Simge Yok
+18 Çalışma alanı simgesi yok
 
 $set 15 #Window
 
@@ -87,44 +208,73 @@ $set 15 #Window
 $set 16 #Windowmenu
 
 1 Kapat
-2 Küçült
-4 Küçült
-5 Ekranı Kapla
-6 Göster
-7 Gönder ...
-8 Şekillendir
-9 Yapıştır
+2 Simge boyutuna küçült
+3 Katman
+4 Düşür
+5 En büyük boya yetir
+6 Büyüt
+7 ...'a gönder
+8 Gölge
+9 Yapışkan
+10 Öldür
+11 Varsayılanları kullan
+12 Pencere başlığını ayarla
 
 $set 17 #Workspace
 
-1 Masaüstü %d
-2 Masaüstleri
-3 Yeni Masaüstü
-4 Son Masaüstünü Kaldır
+1 Çalışma alanı %d
+2 Çalışma Alanları
+3 Yeni çalışma alanı
+4 Sonuncuyu sil
 
 $set 18 #fbsetroot
 
-1 hata : -solid, -mod yada -gradient'den en az birisini belirlemek zorundasınız\n
-3 -display <metin>         bağlantıyı göster\n\
--mod <x> <y>             yerleştirme\n\
--foreground, -fg <renk>  önplan rengi\n\
--background, -bg <renk>  arkaplan rengi\n\n\
--gradient <doku>         geçiş dokusu\n\
--from <renk>             geçiş başlama rengi\n\
--to <renk>               geçiş bitiş rengi\n\n\
--solid <renk>            tek renk\n\n\
--help                    bu yardım iletisini gösterir\n
+1 Hata: Bunlardan en az birini belirtmelisiniz: -solid, -mod, -gradient\n
+2 pixmap atomları oluşturulamıyor, bırakılıyor!
+3 -display <string>        bağlantıyı görünltüler\n\
+-mod <x> <y>             kalıbı ayarlar\n\
+-foreground, -fg <color> önplan rengini ayarlar\n\
+-background, -bg <color> arkaplan rengini ayarlar\n\n\
+-gradient <texture>      doku eğimi\n\
+-from <color>            başlangıç renk eğimi\n\
+-to <color>              bitiş renk eğimi\n\n\
+-solid <color>           rengi katılaştırır\n\n\
+-help                    bu yardım ekranını görüntüler ve çıkar\n
 
 $set 19 #main
 
-1 hata : '-display' bir argüman gerektirir
-11 hata : '-rc' bir argüman gerektirir
-13 Fluxbox %s: (c) %s Henrik Kinnunen\n\n\
--display <metin>\t\tekran bağlantısı kullan.\n\
--rc <metin>\t\t\talternatif bir ayar kaynak dosyası kullan.\n\
--version\t\t\tsürümü göster.\n\
--info\t\t\t\tdisplay some useful information.\n\
--log <filename>\t\t\tlog output to file.\n\
--help\t\t\t\tyardım ekranı.\n\n
+1 hata: '-display' bir parametre ile birlikte kullanılıyor
+2 Kötü ayırma
+3 Kötü biçim
+4 Aralık dışı
+5 Çalışma zamanı hatası
+6 Standart İstisna
+7 Bilinmeyen hata
+8 hata: '-log' bir parametre ile birlikte kullanılıyor
+9 Günlük dosyası
+10 Günlük tutuluyor: 
+11 hata: '-rc' bir parametre ile birlikte kullanılıyor
+12 hata: '-screen' bir parametre ile birlikte kullanılıyor
+13 Fluxbox %s: (c) %s Henrik Kinnunen\n\
+Web sitesi: http://www.fluxbox.org/\n\n\
+-display <string>\t\tekran bağlantısını kullan.\n\
+-screen <all|int,int,int>\tsadece belirlenen ekranlarda çalış.\n\
+-rc <string>\t\t\tulternatif kaynak dosyasını kullan.\n\
+-version\t\t\tversiyonu görüntüle ve çık.\n\
+-info\t\t\t\tyararlı bilgi görüntüler.\n\
+-list-commands\t\t\tbütün geçerli tuş komutlarını listeler.\n\
+-sync\t\t\t\tHata ayıklama için X sunucusu ile eşzamanlar.\n\
+-log <filename>\t\t\tgünlük çıktısını dosyaya yaz..\n\
+-help\t\t\t\tbu yardım ekranını gösterir ve çıkar.\n\n
+14 warning: çevre değişkeni 'DISPLAY' ayarlanamıyor
+
+$set 20 #layers
+
+1 Dok'un üstü
+2 Alt
+3 Masaüstü
+4 Dok
+5 Normal
+6 Üst
 
 
