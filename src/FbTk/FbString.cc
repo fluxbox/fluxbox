fluxbox/fluxbox

commit 690d926ac444243611cd875fb84fabb4e6db2cf2

introduced FbTk::BidiString
a 'BidiString' holds both the logical content and the visual reordered
version of the content of a string. this helps to reduce the number of
calls to reorder the string before drawing it (as introduced in the patch
from Ken Bloom) and to be more consistent in menus and textboxes (drawing
cursors and underlining text).

src/FbTk/FbString.cc
@@ -62,7 +62,85 @@ using std::cerr;
 using std::endl;
 #endif // DEBUG
 
+namespace {
+
+#ifdef HAVE_FRIBIDI
+FbTk::FbString makeVisualFromLogical(const FbTk::FbString& src) {
+
+    FriBidiCharType base = FRIBIDI_TYPE_N;
+
+    // reuse allocated memory for reencoding / reordering
+    static std::vector<FriBidiChar> us;
+    static std::vector<FriBidiChar> out_us;
+    static FbTk::FbString result;
+
+    const size_t S = src.size() + 1;
+    const size_t S4 = S * 4;
+
+    if (us.capacity() < S)
+        us.reserve(S);
+    if (out_us.capacity() < S)
+        out_us.reserve(S);
+    if (result.capacity() < S4)
+        result.reserve(S4);
+
+    us.resize(S);
+    FriBidiStrIndex len = fribidi_charset_to_unicode(FRIBIDI_CHAR_SET_UTF8,
+            const_cast<char*>(src.c_str()), S - 1,
+            &us[0]);
+
+    out_us.resize(S);
+    fribidi_log2vis(&us[0], len, &base, &out_us[0], NULL, NULL, NULL);
+
+    result.resize(S4);
+    len = fribidi_unicode_to_charset(FRIBIDI_CHAR_SET_UTF8, &out_us[0], len, &result[0]);
+    result.resize(len); // trim to currently used chars
+
+    return result;
+}
+
+#endif
+
+} // end of anonymous namespace
+
+
 namespace FbTk {
+
+
+BiDiString::BiDiString(const FbString& logical) 
+#ifdef HAVE_FRIBIDI
+    : m_visual_dirty(false)
+#endif
+{
+    if (!logical.empty())
+        setLogical(logical);
+}
+
+const FbString& BiDiString::setLogical(const FbString& logical) {
+    m_logical = logical;
+#if HAVE_FRIBIDI
+    if (m_logical.empty()) {
+        m_visual_dirty = false;
+        m_visual.clear();
+    } else {
+        m_visual_dirty = true;
+    }
+#endif
+}
+
+const FbString& BiDiString::visual() const {
+#if HAVE_FRIBIDI
+    if (m_visual_dirty) {
+        m_visual = ::makeVisualFromLogical(logical());
+    }
+    m_visual_dirty = false;
+    return m_visual;
+#else
+    return m_logical;
+#endif
+}
+
+
 
 namespace FbStringUtil {
 
@@ -128,7 +206,6 @@ void shutdown() {
 
 
 
-#ifdef HAVE_ICONV
 /**
    Recodes the text from one encoding to another
    assuming cd is correct
@@ -137,7 +214,9 @@ void shutdown() {
    @param size number of BYTES to convert
    @return the recoded string, or 0 on failure
 */
+string recode(iconv_t cd, const string &in) {
 
+#ifdef HAVE_ICONV
 /**
   --NOTE--
   In the "C" locale, this will strip any high-bit characters
@@ -145,8 +224,6 @@ void shutdown() {
   then you need to set your locale to something UTF-8, OR something
   ISO8859-1.
 */
-string recode(iconv_t cd,
-             const string &in) {
 
     // If empty message, yes this can happen, return
     if (in.empty())
@@ -163,18 +240,18 @@ string recode(iconv_t cd,
     size_t inbytesleft = insize;
     size_t outbytesleft = outsize;
 
+#ifdef HAVE_CONST_ICONV
+    const char* in_ptr = in.data();
+#else
     char * in_ptr = const_cast<char *>(in.data());
+#endif
     size_t result = (size_t)(-1);
     bool again = true;
 
     while (again) {
         again = false;
 
-#ifdef HAVE_CONST_ICONV
-        result = iconv(cd, (const char**)(&in_ptr), &inbytesleft, &out_ptr, &outbytesleft);
-#else
         result = iconv(cd, &in_ptr, &inbytesleft, &out_ptr, &outbytesleft);
-#endif  // HAVE_CONST_ICONV
 
         if (result == (size_t)(-1)) {
             switch(errno) {
@@ -213,13 +290,10 @@ string recode(iconv_t cd,
         free(out);
 
     return ret;
-}
 #else
-string recode(int cd,
-             const string &str) {
     return str;
-}
 #endif // HAVE_ICONV
+}
 
 FbString XStrToFb(const string &src) {
     return recode(iconv_convs[X2FB], src);
@@ -249,58 +323,18 @@ bool haveUTF8() {
 }
 
 
-#ifdef HAVE_FRIBIDI
-
-FbString BidiLog2Vis (const FbString& src) {
-
-    FriBidiCharType base = FRIBIDI_TYPE_N;
-
-    // reuse allocated memory for reencoding / reordering
-    static std::vector<FriBidiChar> us;
-    static std::vector<FriBidiChar> out_us;
-    static FbString result;
-
-    const size_t S = src.size() + 1;
-    const size_t S4 = S * 4;
-
-    if (us.capacity() < S)
-        us.reserve(S);
-    if (out_us.capacity() < S)
-        out_us.reserve(S);
-    if (result.capacity() < S4)
-        result.reserve(S4);
-
-    us.resize(S);
-    FriBidiStrIndex len = fribidi_charset_to_unicode(FRIBIDI_CHAR_SET_UTF8,
-            const_cast<char*>(src.c_str()), S - 1,
-            &us[0]);
-
-    out_us.resize(S);
-    fribidi_log2vis(&us[0], len, &base, &out_us[0], NULL, NULL, NULL);
-
-    result.resize(S4);
-    len = fribidi_unicode_to_charset(FRIBIDI_CHAR_SET_UTF8, &out_us[0], len, &result[0]);
-    result.resize(len); // trim to currently used chars
-
-    return result;
-}
-
-#endif
-
 } // end namespace StringUtil
 
-StringConvertor::StringConvertor(EncodingTarget target):
 #ifdef HAVE_ICONV
-    m_iconv((iconv_t)(-1)) {
+StringConvertor::StringConvertor(EncodingTarget target) : m_iconv((iconv_t)(-1)) {
     if (target == ToLocaleStr)
         m_destencoding = FbStringUtil::locale_codeset;
     else
         m_destencoding = "UTF-8";
 }
 #else
-     m_iconv(-1) {}
+StringConvertor::StringConvertor(EncodingTarget target) { }
 #endif
-
 
 StringConvertor::~StringConvertor() {
 #ifdef HAVE_ICONV
@@ -329,11 +363,19 @@ bool StringConvertor::setSource(const string &encoding) {
 #endif
 }
 
-string StringConvertor::recode(const string &src) {
+FbString StringConvertor::recode(const string &src) {
 #ifdef HAVE_ICONV
     return FbStringUtil::recode(m_iconv, src);
 #else
     return src;
+#endif
+}
+
+void StringConvertor::reset() {
+#ifdef HAVE_ICONV
+    if (m_iconv != ((iconv_t)-1))
+        iconv_close(m_iconv);
+    m_iconv = ((iconv_t)(-1));
 #endif
 }
 

src/ClientMenu.cc
@@ -36,7 +36,7 @@ namespace { // anonymous
 class ClientMenuItem: public FbTk::MenuItem {
 public:
     ClientMenuItem(Focusable &client, ClientMenu &menu):
-        FbTk::MenuItem(client.title().c_str(), menu),
+        FbTk::MenuItem(client.title(), menu),
         m_client(client) {
         m_signals.join(client.titleSig(),
                        FbTk::MemFunSelectArg1(menu, &ClientMenu::titleChanged));
@@ -65,7 +65,7 @@ public:
         }
     }
 
-    const std::string &label() const { return m_client.title(); }
+    const FbTk::BiDiString &label() const { return m_client.title(); }
     const FbTk::PixmapWithMask *icon() const {
         return m_client.screen().clientMenuUsePixmap() ? &m_client.icon() : 0;
     }

src/ClientPattern.cc
@@ -341,13 +341,13 @@ bool ClientPattern::addTerm(const string &str, WinProperty prop, bool negate) {
     return true;
 }
 
-string ClientPattern::getProperty(WinProperty prop, const Focusable &client) {
+FbTk::FbString ClientPattern::getProperty(WinProperty prop, const Focusable &client) {
     // we need this for some of the window properties
     const FluxboxWindow *fbwin = client.fbwindow();
 
     switch (prop) {
     case TITLE:
-        return client.title();
+        return client.title().logical();
         break;
     case CLASS:
         return client.getWMClassClass();

src/ClockTool.cc
@@ -93,7 +93,7 @@ timeval calcNextTimeout(const std::string& fmt_string) {
 class ClockMenuItem: public FbTk::MenuItem {
 public:
     explicit ClockMenuItem(ClockTool &tool):
-        FbTk::MenuItem(""), m_tool(tool) {
+        FbTk::MenuItem(FbTk::BiDiString("")), m_tool(tool) {
 
         setClockModeLabel();
         setCloseOnClick(false);
@@ -160,7 +160,7 @@ ClockTool::ClockTool(const FbTk::FbWindow &parent,
                      FbTk::ThemeProxy<ToolTheme> &theme, BScreen &screen,
                      FbTk::Menu &menu):
     ToolbarItem(ToolbarItem::FIXED),
-    m_button(parent, theme->font(), ""),
+    m_button(parent, theme->font(), FbTk::BiDiString("")),
     m_theme(theme),
     m_screen(screen),
     m_pixmap(0),
@@ -243,7 +243,7 @@ void ClockTool::update(FbTk::Subject *subj) {
     // we only replace numbers with zeros because everything else should be
     // relatively static. If we replace all text with zeros then widths of
     // proportional fonts with some strftime formats will be considerably off.
-    std::string text(m_button.text());
+    FbTk::FbString text(m_button.text().logical());
 
     int textlen = text.size();
     for (int i=0; i < textlen; ++i) {
@@ -255,7 +255,7 @@ void ClockTool::update(FbTk::Subject *subj) {
     unsigned int new_width = m_button.width();
     unsigned int new_height = m_button.height();
     translateSize(orientation(), new_width, new_height);
-    new_width = m_theme->font().textWidth(text, text.size());
+    new_width = m_theme->font().textWidth(text.c_str(), text.size());
     translateSize(orientation(), new_width, new_height);
     if (new_width != m_button.width() || new_height != m_button.height()) {
         resize(new_width, new_height);
@@ -296,7 +296,7 @@ void ClockTool::updateTime() {
         if( time_string_len == 0)
             return;
         std::string text = m_stringconvertor.recode(time_string);
-        if (m_button.text() == text)
+        if (m_button.text().logical() == text)
             return;
 
         m_button.setText(text);

src/Ewmh.cc
@@ -131,7 +131,7 @@ void extractNetWmIcon(Atom net_wm_icon, WinClient& winclient) {
         // actually there is some data in _NET_WM_ICON
         nr_icon_data = nr_bytes_left / sizeof(CARD32);
 
-        fbdbg << "extractNetWmIcon: " << winclient.title() << "\n";
+        fbdbg << "extractNetWmIcon: " << winclient.title().logical() << "\n";
         fbdbg << "nr_icon_data: " << nr_icon_data << "\n";
 
         // read all the icons stored in _NET_WM_ICON
@@ -164,7 +164,7 @@ void extractNetWmIcon(Atom net_wm_icon, WinClient& winclient) {
         if (width >= nr_icon_data) {
 
             fbdbg << "Ewmh.cc extractNetWmIcon found strange _NET_WM_ICON width (" 
-                  << width << ") for " << winclient.title() << "\n";
+                  << width << ") for " << winclient.title().logical() << "\n";
             break;
         }
 
@@ -172,7 +172,7 @@ void extractNetWmIcon(Atom net_wm_icon, WinClient& winclient) {
         if (height >= nr_icon_data) {
 
             fbdbg << "Ewmh.cc extractNetWmIcon found strange _NET_WM_ICON height (" 
-                  << height << ") for " << winclient.title() << "\n";
+                  << height << ") for " << winclient.title().logical() << "\n";
 
             break;
         }
@@ -180,7 +180,8 @@ void extractNetWmIcon(Atom net_wm_icon, WinClient& winclient) {
         // strange values stored in the NETWM_ICON
         if (i + width * height > nr_icon_data) {
             fbdbg << "Ewmh.cc extractNetWmIcon found strange _NET_WM_ICON dimensions (" 
-                  << width << "x" << height << ")for " << winclient.title() << "\n";
+
+                  << width << "x" << height << ")for " << winclient.title().logical() << "\n";
 
             break;
         }

src/FbCommands.cc
@@ -364,7 +364,7 @@ void ShowCustomMenuCmd::execute() {
 
 void ShowCustomMenuCmd::reload() {
     m_menu->removeAll();
-    m_menu->setLabel("");
+    m_menu->setLabel(FbTk::BiDiString(""));
     MenuCreator::createFromFile(custom_menu_file, *m_menu.get(), m_menu->reloadHelper());
 }
 

src/FbTk/FbString.hh
@@ -36,6 +36,25 @@ namespace FbTk {
 // (or just plain whatever for now if no utf-8 available)
 typedef std::string FbString;
 
+class BiDiString {
+
+public:
+
+    BiDiString(const FbString& logical = FbString());
+
+    const FbString& logical() const { return m_logical; }
+    const FbString& visual() const;
+
+    const FbString& setLogical(const FbString& logical);
+
+private:
+    FbString m_logical;
+#ifdef HAVE_FRIBIDI
+    mutable FbString m_visual;
+    mutable bool m_visual_dirty;
+#endif
+};
+
 namespace FbStringUtil {
 
 void init();
@@ -52,11 +71,6 @@ std::string FbStrToX(const FbString &src);
 FbString LocaleStrToFb(const std::string &src);
 std::string FbStrToLocale(const FbString &src);
 
-#ifdef HAVE_FRIBIDI
-/// Make Bidi
-FbString BidiLog2Vis (const FbString& src);
-#endif
-
 bool haveUTF8();
 
 } // namespace FbStringUtil
@@ -70,23 +84,14 @@ public:
     ~StringConvertor();
 
     bool setSource(const std::string &encoding);
-    void reset() {
-#ifdef HAVE_ICONV
- if (m_iconv != ((iconv_t)-1))
-     iconv_close(m_iconv);
- m_iconv = ((iconv_t)(-1));
-#endif
-    }
+    void reset();
 
-    std::string recode(const std::string &src);
+    FbString recode(const FbString &src);
 
 private:
 #ifdef HAVE_ICONV
     iconv_t m_iconv;
-#else
-    int m_iconv;
 #endif
-
     std::string m_destencoding;
 };
 

src/FbTk/Font.cc
@@ -247,14 +247,8 @@ bool Font::load(const string &name) {
     return false;
 }
 
-unsigned int Font::textWidth(const FbString &text, unsigned int size) const {
-#ifdef HAVE_FRIBIDI
-    const FbString visualOrder(FbTk::FbStringUtil::BidiLog2Vis(text));
-#else
-    const FbString &visualOrder = text;
-#endif
-
-    return m_fontimp->textWidth(visualOrder, size);
+unsigned int Font::textWidth(const char* text, unsigned int size) const {
+    return m_fontimp->textWidth(text, size);
 }
 
 unsigned int Font::height() const {
@@ -274,34 +268,28 @@ bool Font::validOrientation(FbTk::Orientation orient) {
 }
 
 void Font::drawText(const FbDrawable &w, int screen, GC gc,
-                    const FbString &text, size_t len, int x, int y,
+                    const char* text, size_t len, int x, int y,
                     Orientation orient) const {
-    if (text.empty() || len == 0)
+
+    if (!text || !*text || len == 0)
         return;
-
-#ifdef HAVE_FRIBIDI
-    const FbString visualOrder(FbTk::FbStringUtil::BidiLog2Vis(text));
-#else
-    const FbString &visualOrder = text;
-#endif
-
 
     // draw "effects" first
     if (m_shadow) {
         FbTk::GContext shadow_gc(w);
         shadow_gc.setForeground(m_shadow_color);
-        m_fontimp->drawText(w, screen, shadow_gc.gc(), visualOrder, len,
+        m_fontimp->drawText(w, screen, shadow_gc.gc(), text, len,
                  x + m_shadow_offx, y + m_shadow_offy, orient);
     } else if (m_halo) {
         FbTk::GContext halo_gc(w);
         halo_gc.setForeground(m_halo_color);
-        m_fontimp->drawText(w, screen, halo_gc.gc(), visualOrder, len, x + 1, y + 1, orient);
-        m_fontimp->drawText(w, screen, halo_gc.gc(), visualOrder, len, x - 1, y + 1, orient);
-        m_fontimp->drawText(w, screen, halo_gc.gc(), visualOrder, len, x - 1, y - 1, orient);
-        m_fontimp->drawText(w, screen, halo_gc.gc(), visualOrder, len, x + 1, y - 1, orient);
+        m_fontimp->drawText(w, screen, halo_gc.gc(), text, len, x + 1, y + 1, orient);
+        m_fontimp->drawText(w, screen, halo_gc.gc(), text, len, x - 1, y + 1, orient);
+        m_fontimp->drawText(w, screen, halo_gc.gc(), text, len, x - 1, y - 1, orient);
+        m_fontimp->drawText(w, screen, halo_gc.gc(), text, len, x + 1, y - 1, orient);
     }
 
-    m_fontimp->drawText(w, screen, gc, visualOrder, len, x, y, orient);
+    m_fontimp->drawText(w, screen, gc, text, len, x, y, orient);
 
 
 }

commit b9e9eb4e44a30204f719c40edd792f35305af932

FbString: fix build without iconv
Commit 690d926 (introduced FbTk::BidiString) broke building without
HAVE_ICONV, because of wrong variable name and use of iconv_t type.

Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

src/FbTk/FbString.cc
@@ -150,6 +150,7 @@ enum ConvType { FB2X = 0, X2FB, LOCALE2FB, FB2LOCALE, CONVSIZE };
 #ifdef HAVE_ICONV
 static iconv_t *iconv_convs = 0;
 #else
+typedef int iconv_t;
 static int iconv_convs[CONVSIZE];
 #endif // HAVE_ICONV
 
@@ -292,7 +293,7 @@ string recode(iconv_t cd, const string &in) {
 
     return ret;
 #else
-    return str;
+    return in;
 #endif // HAVE_ICONV
 }
 