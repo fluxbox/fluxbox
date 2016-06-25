dcdde4d32c93d01df205bc06d7dfcbd356be031f
Thomas Lübking <thomas.luebking@gmail.com>
2016-06-25 22:25:48 +0200

    replace FbRootWindow::depth with maxDepth
    The depth member of FbWindow was abused to store the maximum depth
    but that gets overridden with geometry changes of the root window
    (screen layout changes) so we store and read the value explicitly while
    ::depth() maintains the actual depth of the root window
    
    The result of this is that frames for ARGB windows were created with a
    wrong depth and failed to reparent the client window.
    
    BUG: 1102
    BUG: 1058

--- src/Screen.cc
+++ src/Screen.cc
@@ -297,7 +297,7 @@ BScreen::BScreen(FbTk::ResourceManager &rm,
                             "using visual 0x%lx, depth %d\n",
                             "informational message saying screen number (%d), visual (%lx), and colour depth (%d)").c_str(),
             screenNumber(), XVisualIDFromVisual(rootWindow().visual()),
-            rootWindow().depth());
+            rootWindow().maxDepth());
 #endif // DEBUG
 
     FbTk::EventManager *evm = FbTk::EventManager::instance();
