625a690067b694afcde1aee7181dbe6ee6877727
Jan Sucan <jan@jansucan.com>
2020-04-05 10:28:08 +0200

    Add missing scope to a method definition

--- src/SystemTray.cc
+++ src/SystemTray.cc
@@ -135,7 +135,7 @@ private:
     bool m_xembedded; // using xembed protocol? (i.e. unmap when done)
 };
 
-void TrayWindow::pinByClassname(const std::vector<std::string> &left,
+void SystemTray::TrayWindow::pinByClassname(const std::vector<std::string> &left,
         const std::vector<std::string> &right) {
     // based on the parsed order list and a given window sets m_order to
     // an ordinal used to sort the tray icons.
