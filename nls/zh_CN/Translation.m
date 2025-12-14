8f7782b4735ed3f5f72eb1893ce0923d091541cb
ykla <yklaxds@gmail.com>
2025-12-14 09:30:42 +0800

    Update Simplified Chinese translation

--- nls/zh_CN/Translation.m
+++ nls/zh_CN/Translation.m
@@ -19,34 +19,37 @@ $set 1 #Align
 15 左上方
 16 左上方
 17 垂直
+18 居中
+19 顶部
+20 底部
 
 $set 2 #BaseDisplay
 
-1 退出... 存储崩溃文件\n
-2 关闭\n
-3 %s:      捕获信号 %d\n
+1 正在退出…… 崩溃转储中\n
+2 正在关闭\n
+3 %s:      捕获到信号 %d\n
 
 $set 3 #Common
 
-1 Alpha值
+1 Alpha 值
 2 自动隐藏
 3 编译时间
 4 编译选项
 5 编译器
 6 编译器版本
 7 	初始化
-8 	键绑订
+8 	快捷键
 9 	本地语言支持
 10 	菜单
 11 	样式
 12 默认配置
 13 禁用
 14 错误
-15 Fluxbox版本
-16 最大化时盖过
-17 GIT修订版本号
-18 可见
-19 该主题未定义任何背景。\n请参考手册或阅读FAQ。
+15 Fluxbox 版本
+16 覆盖最大化
+17 Git 修订版本号
+18 显示
+19 此样式中未指定背景选项。\n请参考手册或阅读 FAQ。
 
 $set 4 #Configmenu
 
@@ -56,7 +59,7 @@ $set 4 #Configmenu
 4 点击聚焦
 5 装饰临时窗口
 6 鼠标滚轮切换工作区
-7 Focus Window on Workspace Change
+7 在切换工作区时聚焦窗口
 8 聚焦模式
 9 聚焦新窗口
 10 完全最大化
@@ -65,19 +68,23 @@ $set 4 #Configmenu
 14 鼠标聚焦
 15 工作区回旋
 16 强制伪透明
-17 菜单Alpha值
+17 菜单 Alpha 值
 18 透明
-19 聚焦窗口Alpha值
-20 非聚焦窗口Alpha值
+19 聚焦窗口 Alpha 值
+20 非聚焦窗口 Alpha 值
 21 标签选项
 22 标签置于标题栏内
 23 外置标签宽度
 24 点击聚焦标签
 25 鼠标聚焦标签
+26 最大化选项
+27 忽略大小调整步进
+28 禁用移动
+29 禁用调整大小
 
 $set 5 #Ewmh
 
-1 致命错误: 内存不足,无法为EWMH client list分配内存
+1 致命错误: 内存不足，无法为 EWMH client list 分配内存
 
 $set 6 #FbTkError
 
@@ -86,91 +93,92 @@ $set 6 #FbTkError
 3 警告: 不能加载备用字体
 4 分配错误。
 5 创建像素映射出错
-6 无法创建XImage
+6 无法创建 XImage
 7 读取失败
-8 警告：无法为alpha找到有效格式。
-9 警告：创建alpha图片失败。
-10 警告：创建alpha像素映射失败。
-11 查找屏幕格式失败(%d)
+8 警告：未能找到有效的 alpha 格式
+9 警告：创建 alpha 图像失败
+10 警告：创建 alpha pixmap 失败
+11 未能找到屏幕 (%d) 的格式
 12 内存不足
-13 内存不足，无法分配给blue buffer。
-14 内存不足，无法分配给green buffer。
-15 内存不足，无法分配给red buffer。
+13 分配蓝色缓冲区时内存不足
+14 分配绿色缓冲区时内存不足
+15 分配红色缓冲区时内存不足
 16 读取主题项目失败
 17 不支持图像模式
 18 设置默认值
 
 $set 7 #Fluxbox
 
-1 无效的rc文件名!
+1 rc 文件名无效!
 2 加载组文件失败
 3 加载数据库失败
-4 Retrying with
+4 使用其重试
 5 警告！无法找到屏幕来绘制窗口！
-6 无法创建 %s 目录
+6 无法创建目录 %s
 7 无法找到需要控制的屏幕。\n请确保没有其他窗口管理器正在运行。
 8 语法规则解析错误
-9 致命错误！fluxbox类只能有一个实例。
-10 无法连接X server。\n请确保你开启Fluxbox之前开启了X。
-11 警告：X server不支持locale
-12 警告：无法设置locale modifiers
+9 致命错误！fluxbox 类只能有一个实例。
+10 无法连接 X server。\n请确保你在开启 Fluxbox 之前开启了 X。
+11 警告：X server 不支持 locale
+12 警告：无法设置 locale modifiers
 
 $set 8 #Gnome
 
-1 错误: 内存不足, 无法为GNOME client list分配内存
+1 错误: 内存不足, 无法为 GNOME client list 分配内存
 
 $set 9 #Keys
 
-1 Keys: Error on line
-2 Keys: Failed to merge keytree!
-3 Keys: Invalid key/modifier on line
+1 Keys: 行中存在错误
+2 Keys: 键树合并失败！
+3 Keys: 行中键或修饰符无效
 
 $set 10 #Menu
 
 1 配置
-2 Fluxbox默认菜单
+2 Fluxbox 默认菜单
 3 退出
 4 图标
 5 层次...
 6 位于监视器...
 7 位置
 8 重新加载配置文件
-9 重启Fluxbox
-10 警告：[encoding]标签不对称
+9 重启 Fluxbox
+10 警告：[encoding] 标签不对称
 
 $set 11 #Remember
 
 1 装饰
 2 尺寸
-3 跳至工作区
-4 层
-5 记住...
+3 跳转到工作区
+4 层级
+5 记住…
 6 位置
 7 关闭时保存
-8 卷起
-9 粘住
-10 未知按键
+8 阴影化
+9 固定
+10 未知应用快捷键
 11 工作区
-12 监视器
+12 显示器
+13 透明度
 
 $set 12 #Screen
 
-1 BScreen::BScreen: 询问X server时发生错误.\n 另外一个窗口管理器正运行,display为
+1 BScreen::BScreen: 询问 X server 时发生错误.\n 另一个窗口管理器正运行，display 为
 2 W: %4d x H: %4d
 3 BScreen::BScreen: 控制屏幕 %d 使用图像模式 0x%lx, 色彩深度 %d\n
 4 W: %04d x H: %04d
 
 $set 13 #Slit
 
-1 Clients
-2 下卷
-3 上卷
-4 Slit方向
-5 Slit层次
-6 Slit位于监视器
-7 Slit位置
+1 客户端
+2 向下循环
+3 向上循环
+4 Slit 方向
+5 Slit 层级
+6 Slit 所在显示器
+7 Slit 放置位置
 8 Slit
-9 保存Slit列表
+9 保存 Slit 列表
 
 $set 14 #Toolbar
 
@@ -186,8 +194,8 @@ $set 14 #Toolbar
 10 工具栏位置
 11 工具栏
 12 工具栏宽度百分比
-13 时钟：24小时格式
-14 时钟：12小时格式
+13 时钟：24 小时制
+14 时钟：12 小时制
 15 编辑时间显示格式
 16 显示图标
 17 所有未最小化的窗口
@@ -201,14 +209,16 @@ $set 16 #Windowmenu
 
 1 关闭
 2 最小化
-3 层次
-4 降低
+3 层级
+4 置于底层
 5 最大化
-6 提升
-7 发送到...
-8 卷起
-9 粘住
-10 杀死
+6 置于顶层
+7 发送到…
+8 阴影
+9 固定
+10 终止
+11 使用默认值
+12 设置窗口标题
 
 $set 17 #Workspace
 
@@ -219,54 +229,53 @@ $set 17 #Workspace
 
 $set 18 #fbsetroot
 
-1 错误: 必须指定: -solid, -mod, -gradient 三个参数中的一个.\n
-2 无法创建pixmap atoms，放弃！
-3 -display <string>        display connection\n\
--mod <x> <y>             modula pattern\n\
--foreground, -fg <color> modula foreground color\n\
--background, -bg <color> modula background color\n\n\
--gradient <texture>      gradient texture\n\
--from <color>            gradient start color\n\
--to <color>              gradient end color\n\n\
--solid <color>           solid color\n\n\
--help                    显示帮助信息\n
+1 错误: 必须指定: -solid, -mod, -gradient 三个参数中的某个。\n
+2 无法创建 pixmap atoms，已放弃！
+
+3 -display <string>        显示连接\n\
+-mod <x> <y>               模式图案\n\
+-foreground, -fg <color>  模式前景色\n\
+-background, -bg <color>  模式背景色\n\n\
+-gradient <texture>        渐变纹理\n\
+-from <color>              渐变起始颜色\n\
+-to <color>                渐变结束颜色\n\n\
+-solid <color>             纯色\n\n\
+-help                      显示帮助信息\n\
 
 $set 19 #main
 
-1 错误: '-display'需要一个参数
-2 Bad Alloc
-3 Bad cast
+1 错误: '-display' 需要一个参数
+2 内存分配失败
+3 类型转换错误
 4 超出显示范围
 5 运行时错误
 6 标准例外
 7 未知错误
-8 错误: '-log'需要一个参数
+8 错误: '-log' 需要一个参数
 9 日志文件
 10 记录日志到
-11 错误: '-rc'需要一个参数
-12 错误: '-screen'需要一个参数
-13 Fluxbox %s: (c) %s Fluxbox Team\n\
+11 错误: '-rc' 需要一个参数
+12 错误: '-screen' 需要一个参数
+13 Fluxbox %s: (c) %s Fluxbox 团队\n\
 主页: http://www.fluxbox.org/\n\n\
--display <string>\t\t指定display连接。\n\
+-display <string>\t\t指定 display 连接。\n\
 -screen <all|int,int,int>\t在特定的屏幕上显示。\n\
--no-slit\t\t\tdo not provide a slit.\n\
--no-toolbar\t\t\tdo not provide a toolbar.\n\
+-no-slit\t\t\t不要留缝隙。\n\
+-no-toolbar\t\t\t不要提供工具栏。\n\
 -rc <string>\t\t\t使用指定的资源文件。\n\
 -version\t\t\t显示版本号然后退出。\n\
 -info\t\t\t\t显示一些有用的信息。\n\
--list-commands\t\t\tlist all valid key commands.\n\
--sync\t\t\t\tsynchronize with X server for debugging.\n\
+-list-commands\t\t\t列出全部有效的按键命令。\n\
+-sync\t\t\t\t为调试而与 X 服务器同步。\n\
 -log <filename>\t\t\t将输出记录到文件中。\n\
 -help\t\t\t\t显示这则帮助信息然后退出。\n\n
-14 警告: 无法设置环境变量'DISPLAY'
+14 警告: 无法设置环境变量 'DISPLAY'
 
 $set 20
 
-1 在Dock之上
+1 在 Dock 之上
 2 底部
 3 桌面
 4 Dock
 5 普通
 6 顶部
-
-
